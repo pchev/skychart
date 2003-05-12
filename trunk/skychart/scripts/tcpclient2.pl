3dbd7da5340bfa0fbde68a87aa85d04e201d3b4a
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2003-05-12 16:58:14 +0000

    Perl client script

---
+++ trunk/skychart/scripts/tcpclient2.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl -w
+use IO::Socket;
+
+#
+# example program to send sequential command to the program
+#
+
+$host = "192.168.0.1";
+$port = "3292";
+$eol = "\x0D\x0A";
+
+  connectCDC();
+
+  sendcmd("proj equat");
+  sendcmd("search M37");
+  sleep(2);
+  sendcmd("fov 3d0m0s");
+
+  $path = `pwd`;
+  chop $path;
+  sendcmd("saveimg PNG $path/test.png");
+  sendcmd("saveimg JPEG $path/test.jpg 50");
+
+  sleep(5);
+  sendcmd("quit");
+
+
+sub sendcmd {
+  my $cmd = shift;
+  print STDOUT " Send CMD to $client: $cmd \n";
+  print $handle $cmd.$eol;                       # send command
+
+  $line = <$handle>;
+  if ($line =~ /$client/) {       # click form our client
+     print STDOUT $line;
+     }
+  while (($line =~/^\.\r\n$/) or ($line =~ /^> /)) # keepalive and click on the chart
+    {
+     $line = <$handle>;
+     if ($line =~ /$client/) {       # click form our client
+        print STDOUT $line;
+        }
+    }
+  # we go here after receiving response from our command
+  if (($line =~ /^OK/) or ($line =~ /^Bye/) )
+     {
+     print STDOUT "Command success\n";
+     }
+  else {
+     print STDOUT "Command failed\n";
+     }
+}
+
+sub connectCDC {
+
+# do the connection
+$handle = IO::Socket::INET->new(Proto     => "tcp",
+                                PeerAddr  => $host,
+                                PeerPort  => $port)
+          or die "cannot connect to Cartes du Ciel at $host port $port : $!";
+
+$handle->autoflush(1);
+
+print STDOUT "[Connected to $host:$port]\n";
+
+# wait connection and get client chart name
+  $line = <$handle>;
+  print STDOUT $line;
+  $line =~ /OK (.*)$/;
+  $client = $1;
+  chop $client;
+  if ($client)
+    {
+     print STDOUT " We are connected to $client \n";
+    }
+    else { die " We are not connected \n"};
+}
---
+++ trunk/skychart/scripts/tcpclient3.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl -w
+
+#
+# example program that fork to Send and Receive process.
+# work the same way as telnet.
+#
+
+use strict;
+use IO::Socket;
+my ($host, $port, $kidpid, $handle, $line, $eol);
+
+$host = "192.168.0.1";
+$port = "3292";
+
+$eol = "\x0D\x0A";
+
+# do the connection
+$handle = IO::Socket::INET->new(Proto     => "tcp",
+                                PeerAddr  => $host,
+                                PeerPort  => $port)
+          or die "cannot connect to Cartes du Ciel at $host port $port : $!";
+
+$handle->autoflush(1);
+
+print STDERR "[Connected to $host:$port]\n";
+
+# fork the Send and Receive part:
+die "cannot fork: $!" unless defined($kidpid = fork());
+
+# main process with the Receive function:
+if ($kidpid) {
+   # print each received line
+   while (defined ($line = <$handle>)) {
+          if (!($line =~/^\.\r\n$/))  #skip keepalive data
+             {
+             print STDOUT $line;
+             }
+          }
+   kill("TERM", $kidpid);          # kill Send process on exit
+   }
+
+   # child process with the Send function:
+   else {
+        # send interactive command :
+         while (defined ($line = <STDIN>))
+         {
+           chop $line;
+           print $handle $line.$eol;
+         }
+}
