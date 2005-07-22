ba61f968a6b8ba37ad4fdbdb0a40cee9eebe46d6
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2005-07-22 19:58:32 +0000

    GetDSS support to extract images from RealSky cdrom

---
+++ trunk/skychart/pu_getdss.pas
@@ -0,0 +1,74 @@
+unit pu_getdss;
+
+interface
+
+uses u_constant, u_util, Math,
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  Grids, StdCtrls, Buttons;
+
+// GetDss.dll interface
+  type
+  SImageConfig = record
+     pDir : Pchar;
+     pDrive : Pchar;
+     pImageFile : Pchar;
+     DataSource : integer;
+     PromptForDisk : boolean;
+     SubSample : integer;
+     Ra : double;
+     De : double;
+     Width : double;
+     Height : double;
+     Sender : Thandle;
+     pApplication :Pchar;
+     pPrompt1 : Pchar;
+     pPrompt2 : Pchar;
+  end;
+  Plate_data = packed record
+   nplate : integer;
+   plate_name, gsc_plate_name : array[1..10]of Pchar;
+   dist_from_edge, cd_number, is_uk_survey : array[1..10]of integer;
+   year_imaged, exposure : array[1..10]of double;
+  end;
+  PImageConfig = ^SImageConfig;
+  PPlate_data = ^Plate_data;
+
+  TImageExtract=function( img : PImageConfig): Integer; cdecl;
+  TGetPlateList=function( img : PImageConfig; pl : PPlate_data): Integer; cdecl;
+  TImageExtractFromPlate=function( img : PImageConfig; ReqPlateName : Pchar): Integer; cdecl;
+
+
+type
+  Tf_getdss = class(TForm)
+    ListBox1: TListBox;
+    Label1: TLabel;
+    BitBtn1: TBitBtn;
+    BitBtn2: TBitBtn;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+    { Private declarations }
+    dsslib: Dword;
+    Fenabled: boolean;
+    ImageExtract: TImageExtract;
+    GetPlateList: TGetPlateList;
+    ImageExtractFromPlate: TImageExtractFromPlate;
+  public
+    { Public declarations }
+    cfgdss: conf_dss;
+    function GetDss(ra,de,fov,ratio:double):boolean;
+    property enabled: boolean read Fenabled;
+  end;
+
+  Var f_getdss: Tf_getdss;
+
+  Const dsslibname = 'getdss.dll';
+
+
+implementation
+
+{$R *.dfm}
+
+{$include i_getdss.pas}
+
+end.
