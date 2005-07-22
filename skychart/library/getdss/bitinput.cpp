a12f2eda2d7d64848a010fef40f824b5fb9a0ee0
Patrick Chevalley <pch@ap-i.net>
2005-07-22 20:28:13 +0000

    GetDSS library adapted from Bill Gray version. See http://www.projectpluto.com/get_dss.htm

---
+++ skychart/library/getdss/bitinput.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bitfile.h"
+
+/* INPUT A BIT */
+
+int dss_debug_printf( const char *format, ...);        /* extr_fit.cpp */
+
+int input_bit( BITFILE *infile)
+{
+   int rval;
+
+   if( !infile->bit_loc)      /* gotta read the next byte */
+      if( infile->loc == infile->endptr - 1)
+                           /* attempt to read past EOF */
+         infile->error_code = -1;
+      else
+         {
+         infile->bit_loc = 8;
+         infile->loc++;
+         }
+   if( infile->error_code)
+      {
+      dss_debug_printf( "DANGER! (1)\n");
+      exit( -1);
+      }
+   infile->bit_loc--;
+   rval = (infile->loc[0] >> infile->bit_loc) & 1;
+   return( rval);
+}
+
+int input_nbits( BITFILE *infile, const int n)
+{
+   int rval;
+
+   if( (int)infile->bit_loc >= n)
+      rval = infile->loc[0];
+   else
+      if( infile->loc == infile->endptr - 1)
+         {                 /* attempt to read past EOF */
+         infile->error_code = -1;
+         dss_debug_printf(  "DANGER! (2)\n");
+         exit( -2);
+         rval = 0;
+         }
+      else          /* crosses an 8-bit boundary */
+         {
+         rval = (infile->loc[0] << 8) | infile->loc[1];
+         infile->loc++;
+         infile->bit_loc += 8;
+         }
+   infile->bit_loc -= n;
+   /* clear higher bits    */
+   return( (rval >> infile->bit_loc) & ((1<<n)-1) );
+}
