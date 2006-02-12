f5bff30da8e7c3fe5fa2f0d6ea93a2a9efb48ff0
Patrick Chevalley <pch@ap-i.net>
2006-02-12 21:45:03 +0000

    Synapse package for Lazarus

---
+++ skychart/component/synapse/source/lib/synapse.pas
@@ -0,0 +1,23 @@
+{ This file was automatically created by Lazarus. Do not edit!
+This source is only used to compile and install the package.
+ }
+
+unit synapse; 
+
+interface
+
+uses
+  asn1util, blcksock, dnssend, ftpsend, ftptsend, httpsend, imapsend, ldapsend, 
+    mimeinln, mimemess, mimepart, nntpsend, pingsend, pop3send, slogsend, 
+    smtpsend, snmpsend, sntpsend, synachar, synacode, synafpc, synaicnv, 
+    synamisc, synautil, synsock, tlntsend, LazarusPackageIntf; 
+
+implementation
+
+procedure Register; 
+begin
+end; 
+
+initialization
+  RegisterPackage('synapse', @Register); 
+end.
