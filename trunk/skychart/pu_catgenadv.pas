01ab66057493d415b152df21e3c1b6ce9c100c62
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2006-04-22 20:30:20 +0000

    Catgen adjustement for Linux.

--- trunk/skychart/pu_catgenadv.pas
+++ trunk/skychart/pu_catgenadv.pas
@@ -25,7 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 interface
 
 uses
-  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  {Windows,} Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
   StdCtrls, Buttons, LResources;
 
 type
