2d6bbe4b3463306aacee7aef2d26b9627de6e9be
Patrick Chevalley <pch@ap-i.net>
2006-05-21 19:39:51 +0000

    remove component not compatible with FreePascal

--- skychart/component/libsql/sqlcomponents.pas
+++ skychart/component/libsql/sqlcomponents.pas
@@ -6,7 +6,7 @@ unit sqlcomponents;
 
 interface
 
-uses Classes, passql, pasmysql, passqlite, pasthreadedsqlite, pasodbc {$IFNDEF UNIX}, pasjansql, lsdatasetbase, lsdatasetquery, lsdatasettable{$ENDIF};
+uses Classes, passql, pasmysql, passqlite, pasthreadedsqlite, pasodbc {$IFNDEF FPC}{$IFNDEF UNIX}, pasjansql, lsdatasetbase, lsdatasetquery, lsdatasettable{$ENDIF}{$ENDIF};
 
 procedure Register;
 
@@ -14,7 +14,7 @@ implementation
 
 procedure Register;
 begin
-  RegisterComponents('libsql', [TMyDB, TLiteDB, TODBCDB, {$IFNDEF UNIX}TJanDB, TlsTable, TlsQuery,{$ENDIF} TMLiteDB]);
+  RegisterComponents('libsql', [TMyDB, TLiteDB, TODBCDB, {$IFNDEF FPC}{$IFNDEF UNIX}TJanDB, TlsTable, TlsQuery,{$ENDIF}{$ENDIF} TMLiteDB]);
 end;
 
 
