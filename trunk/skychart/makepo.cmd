82f8ea32eaf31489b20d1f64b6773a6064cfbe3c
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2006-07-19 17:53:21 +0000

    update .po files for Windows and Linux

--- trunk/skychart/makepo.cmd
+++ trunk/skychart/makepo.cmd
@@ -1,4 +1,8 @@
-REM run this script to update all the translations after modification of u_translation.pas   
+REM Run this script to update all the translations after modification of a
+REM resource string in u_translation.pas and compilation of the program.
+
+REM Update first the path to your Lazarus installation and run "make" in lazarus/tools
+
 
 C:\appli\lazarus\pp\bin\i386-win32\rstconv -i units\u_translation.rst -o language\skychart.po
 C:\appli\lazarus\tools\updatepofiles language\skychart.po
