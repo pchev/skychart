5cfa3a672ac8422f41c1245a8ed7f05a325b5100
Patrick Chevalley <pch@ap-i.net>
2006-07-20 09:41:33 +0000

    change to my drive installation

--- skychart/makepo.cmd
+++ skychart/makepo.cmd
@@ -4,7 +4,7 @@ REM resource string in u_translation.pas and compilation of the program.
 REM Update first the path to your Lazarus installation and run "make" in lazarus/tools
 
 
-C:\appli\lazarus\pp\bin\i386-win32\rstconv -i units\u_translation.rst -o language\skychart.po
-C:\appli\lazarus\tools\updatepofiles language\skychart.po
+D:\appli\lazarus\pp\bin\i386-win32\rstconv -i units\u_translation.rst -o language\skychart.po
+D:\appli\lazarus\tools\updatepofiles language\skychart.po
 
 pause
