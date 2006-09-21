185b7e81db9bb6fbdb27a1259ae315131ac4dae8
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2006-09-21 19:06:36 +0000

    IniFiles correction definitively include in FPC 2.04 / Lazarus 0.9.18, no more need for cdc_inifiles

--- trunk/skychart/pu_calendar.pas
+++ trunk/skychart/pu_calendar.pas
@@ -27,7 +27,7 @@ interface
 
 uses u_translation, Math, cu_database, Printers,
   LCLIntf, SysUtils, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, FileCtrl, enhedits, Grids, ComCtrls, CDC_IniFiles,
+  Dialogs, StdCtrls, FileCtrl, enhedits, Grids, ComCtrls, IniFiles,
   jdcalendar, cu_planet, u_constant, pu_image, Buttons, ExtCtrls,
   ActnList, StdActns, LResources;
 
