62ba3ba0f6967643d758a33bc23c4110131397d5
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2006-09-28 20:09:37 +0000

    Synapse release 37

--- trunk/skychart/component/synapse/source/lib/mimepart.pas
+++ trunk/skychart/component/synapse/source/lib/mimepart.pas
@@ -1,5 +1,5 @@
 {==============================================================================|
-| Project : Ararat Synapse                                       | 002.007.023 |
+| Project : Ararat Synapse                                       | 002.007.005 |
 |==============================================================================|
 | Content: MIME support procedures and functions                               |
 |==============================================================================|
@@ -536,6 +536,7 @@ begin
   Result.DefaultCharset := FDefaultCharset;
   FSubParts.Add(Result);
   Result.SubLevel := FSubLevel + 1;
+  Result.MaxSubLevel := FMaxSubLevel;  
 end;
 
 {==============================================================================}
@@ -756,12 +757,16 @@ begin
   if FConvertCharset and (FPrimaryCode = MP_TEXT) then
     if (not FForcedHTMLConvert) and (uppercase(FSecondary) = 'HTML') then
     begin
+      b := false;
       t := uppercase(s);
       t := SeparateLeft(t, '</HEAD>');
-      t := SeparateRight(t, '<HEAD>');
-      t := ReplaceString(t, '"', '');
-      t := ReplaceString(t, ' ', '');
-      b := Pos('HTTP-EQUIV=CONTENT-TYPE', t) > 0;
+      if length(t) <> length(s) then
+      begin
+        t := SeparateRight(t, '<HEAD>');
+        t := ReplaceString(t, '"', '');
+        t := ReplaceString(t, ' ', '');
+        b := Pos('HTTP-EQUIV=CONTENT-TYPE', t) > 0;
+      end;
       if not b then
         s := CharsetConversion(s, FCharsetCode, FTargetCharset);
     end
--- trunk/skychart/component/synapse/source/lib/synautil.pas
+++ trunk/skychart/component/synapse/source/lib/synautil.pas
@@ -1,5 +1,5 @@
 {==============================================================================|
-| Project : Ararat Synapse                                       | 004.009.001 |
+| Project : Ararat Synapse                                       | 004.010.001 |
 |==============================================================================|
 | Content: support procedures and functions                                    |
 |==============================================================================|
@@ -71,12 +71,6 @@ uses
   System.IO,
 {$ENDIF}
   SysUtils, Classes, SynaFpc;
-
-type
-{:binary form of IPv6 adress (for string conversion routines)}
-  TIp6Bytes = array [0..15] of Byte;
-{:binary form of IPv6 adress (for string conversion routines)}
-  TIp6Words = array [0..7] of Word;
 
 {:Return your timezone bias from UTC time in minutes.}
 function TimeZoneBias: integer;
@@ -162,27 +156,6 @@ function CodeLongInt(Value: LongInt): Ansistring;
 {:Decodes four characters located at "Index" offset position of the "Value"
  string to LongInt values.}
 function DecodeLongInt(const Value: Ansistring; Index: Integer): LongInt;
-
-{:Returns @TRUE, if "Value" is a valid IPv4 address. Cannot be a symbolic Name!}
-function IsIP(const Value: string): Boolean;
-
-{:Returns @TRUE, if "Value" is a valid IPv6 address. Cannot be a symbolic Name!}
-function IsIP6(const Value: string): Boolean;
-
-{:Returns a string with the "Host" ip address converted to binary form.}
-function IPToID(Host: string): string;
-
-{:Convert IPv6 address from their string form to binary byte array.}
-function StrToIp6(value: string): TIp6Bytes;
-
-{:Convert IPv6 address from binary byte array to string form.}
-function Ip6ToStr(value: TIp6Bytes): string;
-
-{:Convert IPv4 address from their string form to binary.}
-function StrToIp(value: string): integer;
-
-{:Convert IPv4 address from binary to string form.}
-function IpToStr(value: integer): string;
 
 {:Dump binary buffer stored in a string to a result string.}
 function DumpStr(const Buffer: Ansistring): string;
@@ -927,265 +900,6 @@ end;
 
 {==============================================================================}
 
-function IsIP(const Value: string): Boolean;
-var
-  TempIP: string;
-  function ByteIsOk(const Value: string): Boolean;
-  var
-    x, n: integer;
-  begin
-    x := StrToIntDef(Value, -1);
-    Result := (x >= 0) and (x < 256);
-    // X may be in correct range, but value still may not be correct value!
-    // i.e. "$80"
-    if Result then
-      for n := 1 to length(Value) do
-        if not (Value[n] in ['0'..'9']) then
-        begin
-          Result := False;
-          Break;
-        end;
-  end;
-begin
-  TempIP := Value;
-  Result := False;
-  if not ByteIsOk(Fetch(TempIP, '.')) then
-    Exit;
-  if not ByteIsOk(Fetch(TempIP, '.')) then
-    Exit;
-  if not ByteIsOk(Fetch(TempIP, '.')) then
-    Exit;
-  if ByteIsOk(TempIP) then
-    Result := True;
-end;
-
-{==============================================================================}
-
-function IsIP6(const Value: string): Boolean;
-var
-  TempIP: string;
-  s,t: string;
-  x: integer;
-  partcount: integer;
-  zerocount: integer;
-  First: Boolean;
-begin
-  TempIP := Value;
-  Result := False;
-  partcount := 0;
-  zerocount := 0;
-  First := True;
-  while tempIP <> '' do
-  begin
-    s := fetch(TempIP, ':');
-    if not(First) and (s = '') then
-      Inc(zerocount);
-    First := False;
-    if zerocount > 1 then
-      break;
-    Inc(partCount);
-    if s = '' then
-      Continue;
-    if partCount > 8 then
-      break;
-    if tempIP = '' then
-    begin
-      t := SeparateRight(s, '%');
-      s := SeparateLeft(s, '%');
-      x := StrToIntDef('$' + t, -1);
-      if (x < 0) or (x > $ffff) then
-        break;
-    end;
-    x := StrToIntDef('$' + s, -1);
-    if (x < 0) or (x > $ffff) then
-      break;
-    if tempIP = '' then
-      Result := True;
-  end;
-end;
-
-{==============================================================================}
-function IPToID(Host: string): string;
-var
-  s: string;
-  i, x: Integer;
-begin
-  Result := '';
-  for x := 0 to 3 do
-  begin
-    s := Fetch(Host, '.');
-    i := StrToIntDef(s, 0);
-    Result := Result + Chr(i);
-  end;
-end;
-
-{==============================================================================}
-
-function StrToIp(value: string): integer;
-var
-  s: string;
-  i, x: Integer;
-begin
-  Result := 0;
-  for x := 0 to 3 do
-  begin
-    s := Fetch(value, '.');
-    i := StrToIntDef(s, 0);
-    Result := (256 * Result) + i;
-  end;
-end;
-
-{==============================================================================}
-
-function IpToStr(value: integer): string;
-var
-  x1, x2: word;
-  y1, y2: byte;
-begin
-  Result := '';
-  x1 := value div $10000;
-  x2 := value mod $10000;
-  y1 := x1 div $100;
-  y2 := x1 mod $100;
-  Result := inttostr(y1) + '.' + inttostr(y2) + '.';
-  y1 := x2 div $100;
-  y2 := x2 mod $100;
-  Result := Result + inttostr(y1) + '.' + inttostr(y2);
-end;
-
-{==============================================================================}
-
-function StrToIp6(Value: string): TIp6Bytes;
-var
- IPv6: TIp6Words;
- Index: Integer;
- ZeroAt: Integer;
- n: integer;
- b1, b2: byte;
- s: string;
- x: integer;
-begin
-  for n := 0 to 15 do
-    Result[n] := 0;
-  for n := 0 to 7 do
-    Ipv6[n] := 0;
-  Index := 0;
-  ZeroAt := -1;
-
-  while Value <> '' do
-  begin
-    if Index > 7 then
-      Exit;
-    s := fetch(value, ':');
-    if s = '@' then
-      break;
-    if s = '' then
-    begin
-      ZeroAt := Index;
-      IPv6[Index] := 0;
-    end
-    else
-    begin
-      x := StrToIntDef('$' + s, -1);
-      if (x > 65535) or (x < 0) then
-        Exit;
-      IPv6[Index] := x;
-    end;
-    Inc(Index);
-  end;
-  if ZeroAt >= 0 then
-  Begin
-    x := Index - ZeroAt - 1;
-    for n := 1 to x do
-      IPv6[7 - n + 1] := Ipv6[ZeroAt + x - 1 + n];
-    for n := ZeroAt + 1 to Index - 1 do
-      IPv6[n] := 0;
-  End;
-  for n := 0 to 7 do
-  begin
-    b1 := ipv6[n] div 256;
-    b2 := ipv6[n] mod 256;
-    Result[n * 2] := b1;
-    Result[(n * 2) + 1] := b2;
-  end;
-end;
-
-{==============================================================================}
-//based on routine by the Free Pascal development team
-function Ip6ToStr(value: TIp6Bytes): string;
-var
-  i, x: byte;
-  zr1,zr2: set of byte;
-  zc1,zc2: byte;
-  have_skipped: boolean;
-  ip6w: TIp6words;
-begin
-  zr1 := [];
-  zr2 := [];
-  zc1 := 0;
-  zc2 := 0;
-  for i := 0 to 7 do
-  begin
-    x := i * 2;
-    ip6w[i] := value[x] * 256 + value[x + 1];
-    if ip6w[i] = 0 then
-    begin
-      include(zr2, i);
-      inc(zc2);
-    end
-    else
-    begin
-      if zc1 < zc2 then
-      begin
-        zc1 := zc2;
-        zr1 := zr2;
-        zc2 := 0;
-        zr2 := [];
-      end;
-    end;
-  end;
-  if zc1 < zc2 then
-  begin
-    zc1 := zc2;
-    zr1 := zr2;
-  end;
-  SetLength(Result, 8*5-1);
-  SetLength(Result, 0);
-  have_skipped := false;
-  for i := 0 to 7 do
-  begin
-    if not(i in zr1) then
-    begin
-      if have_skipped then
-      begin
-        if Result = '' then
-          Result := '::'
-        else
-          Result := Result + ':';
-        have_skipped := false;
-      end;
-      Result := Result + IntToHex(Ip6w[i], 1) + ':';
-    end
-    else
-    begin
-      have_skipped := true;
-    end;
-  end;
-  if have_skipped then
-    if Result = '' then
-      Result := '::0'
-    else
-      Result := Result + ':';
-
-  if Result = '' then
-    Result := '::0';
-  if not (7 in zr1) then
-    SetLength(Result, Length(Result)-1);
-  Result := LowerCase(result);
-end;
-
-{==============================================================================}
-
 function DumpStr(const Buffer: Ansistring): string;
 var
   n: Integer;
@@ -1253,6 +967,9 @@ function TrimSPLeft(const S: string): string;
 var
   I, L: Integer;
 begin
+  Result := '';
+  if S = '' then
+    Exit;
   L := Length(S);
   I := 1;
   while (I <= L) and (S[I] = ' ') do
@@ -1266,6 +983,9 @@ function TrimSPRight(const S: string): string;
 var
   I: Integer;
 begin
+  Result := '';
+  if S = '' then
+    Exit;
   I := Length(S);
   while (I > 0) and (S[I] = ' ') do
     Dec(I);
@@ -1887,7 +1607,7 @@ end;
 
 procedure HeadersToList(const Value: TStrings);
 var
-  n, x: integer;
+  n, x, y: integer;
   s: string;
 begin
   for n := 0 to Value.Count -1 do
@@ -1896,8 +1616,12 @@ begin
     x := Pos(':', s);
     if x > 0 then
     begin
-      s[x] := '=';
-      Value[n] := s;
+      y:= Pos('=',s); 
+      if not ((y > 0) and (y < x)) then
+      begin
+        s[x] := '=';
+        Value[n] := s;
+      end;
     end;
   end;
 end;
