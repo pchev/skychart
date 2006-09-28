256c084690b6d356d08b1cbfd6f05384241b1eee
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2006-09-28 20:19:12 +0000

    adapt translation script for cross compiler

--- trunk/skychart/makepo.cmd
+++ trunk/skychart/makepo.cmd
@@ -4,7 +4,7 @@ REM resource string in u_translation.pas and compilation of the program.
 REM Update first the path to your Lazarus installation and run "make" in lazarus/tools
 
 
-D:\appli\lazarus\pp\bin\i386-win32\rstconv -i units\u_translation.rst -o language\skychart.po
+D:\appli\lazarus\pp\bin\i386-win32\rstconv -i units\win32\u_translation.rst -o language\skychart.po
 D:\appli\lazarus\tools\updatepofiles language\skychart.po
 
 pause
