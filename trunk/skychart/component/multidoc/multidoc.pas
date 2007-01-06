fbda86bc800b9f6bd04c3131508dad71c07b751e
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2007-01-06 13:18:23 +0000

    Multidoc 0.2

--- trunk/skychart/component/multidoc/multidoc.pas
+++ trunk/skychart/component/multidoc/multidoc.pas
@@ -1,11 +1,22 @@
 unit MultiDoc;
 
-{ Copyright (C) 2005 Patrick Chevalley
+{ Copyright (C) 2007 Patrick Chevalley
 
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Library General Public License as published by
   the Free Software Foundation; either version 2 of the License, or (at your
-  option) any later version.
+  option) any later version with the following modification:
+
+  As a special exception, the copyright holders of this library give you
+  permission to link this library with independent modules to produce an
+  executable, regardless of the license terms of these independent modules,and
+  to copy and distribute the resulting executable under terms of your choice,
+  provided that you also meet, for each linked independent module, the terms
+  and conditions of the license of that module. An independent module is a
+  module which is not derived from or based on this library. If you modify
+  this library, you may extend this exception to your version of the library,
+  but you are not obligated to do so. If you do not wish to do so, delete this
+  exception statement from your version.
 
   This program is distributed in the hope that it will be useful, but WITHOUT
   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
