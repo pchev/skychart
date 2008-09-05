aba1524b6f10e455442e51f844a142d214330fbf
Patrick Chevalley <pch@ap-i.net>
2008-09-05 09:10:45 +0000

    modified by lazarus 0.9.25

--- skychart/library/catalog/cdccatalog.pas
+++ skychart/library/catalog/cdccatalog.pas
@@ -1,5 +1,5 @@
-{ This file was automatically created by Lazarus. Do not edit!
-This source is only used to compile and install the package.
+{ This file was automatically created by Lazarus. do not edit!
+  This source is only used to compile and install the package.
  }
 
 unit cdccatalog; 
@@ -8,9 +8,9 @@ interface
 
 uses
   bscunit, dscat, findunit, gcatunit, gcmunit, gcvunit, gpnunit, gsccompact, 
-    gscconst, gscfits, gscunit, lbnunit, microcatunit, ngcunit, oclunit, 
-    pgcunit, sacunit, skylibcat, skyunit, ticunit, tyc2unit, tycunit, 
-    usnoaunit, wdsunit, rc3unit; 
+  gscconst, gscfits, gscunit, lbnunit, microcatunit, ngcunit, oclunit, 
+  pgcunit, sacunit, skylibcat, skyunit, ticunit, tyc2unit, tycunit, usnoaunit, 
+  wdsunit, rc3unit;
 
 implementation
 
