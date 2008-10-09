281ed42143c30f210a1274cd4a746075724f6050
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2008-10-09 19:09:49 +0000

    install script first try

--- trunk/install.sh
+++ trunk/install.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-echo Install
+destdir=$1
+
+if [ -z "$destdir" ]; then
+   export destdir=/tmp/skychart
+fi
+
+echo Install skychart to $destdir
+
+install -m 755 -d $destdir/bin
+install -m 755 -d $destdir/lib
+install -m 755 -d $destdir/share
+install -m 755 -d $destdir/share/apps
+install -m 755 -d $destdir/share/apps/skychart
+
+for f in $(cat data.lst)
+do
+  install -v -D -m 644  tools/$f $destdir/share/apps/skychart/$f
+done
+
+for f in $(cat doc.lst)
+do
+  install -v -D -m 644  tools/$f $destdir/share/apps/skychart/$f
+done
+
+for f in $(cat cat.lst)
+do
+  install -v -D -m 644  tools/$f $destdir/share/apps/skychart/$f
+done
