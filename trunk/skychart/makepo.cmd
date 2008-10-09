d73d751706e38472ed413925d5d5075e064543bb
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2008-10-09 17:29:16 +0000

    Move translation sources to tools/data/language

--- trunk/skychart/makepo.cmd
+++ trunk/skychart/makepo.cmd
@@ -4,10 +4,10 @@ REM resource string in u_translation.pas and compilation of the program.
 REM Update first the path to your Lazarus installation and run "make" in lazarus/tools
 
 
-D:\appli\lazarus\pp\bin\i386-win32\rstconv -i units\win32\u_translation.rst -o language\skychart.po
-D:\appli\lazarus\tools\updatepofiles language\skychart.po
+D:\appli\lazarus\pp\bin\i386-win32\rstconv -i units\win32\u_translation.rst -o ..\tools\data\language\skychart.po
+D:\appli\lazarus\tools\updatepofiles ..\tools\data\language\skychart.po
 
-D:\appli\lazarus\pp\bin\i386-win32\rstconv -i units\win32\u_help.rst -o language\help.po
-D:\appli\lazarus\tools\updatepofiles language\help.po
+D:\appli\lazarus\pp\bin\i386-win32\rstconv -i units\win32\u_help.rst -o ..\tools\data\language\help.po
+D:\appli\lazarus\tools\updatepofiles ..\tools\data\language\help.po
 
 pause
