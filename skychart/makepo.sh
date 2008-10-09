b9503fc1165af87a18297a6d6b9c786e34b1ca4d
Patrick Chevalley <pch@ap-i.net>
2008-10-09 17:29:16 +0000

    Move translation sources to tools/data/language

--- skychart/makepo.sh
+++ skychart/makepo.sh
@@ -5,8 +5,8 @@
 
 # Update first the path to your Lazarus installation and run "make" in lazarus/tools
 
-rstconv -i units/i386-linux-gtk2/u_translation.rst -o language/skychart.po
-~/lazarus/tools/updatepofiles language/skychart.po
+rstconv -i units/i386-linux-gtk2/u_translation.rst -o ../tools/data/language/skychart.po
+~/lazarus/tools/updatepofiles ../tools/data/language/skychart.po
 
-rstconv -i units/i386-linux-gtk2/u_help.rst -o language/help.po
-~/lazarus/tools/updatepofiles language/help.po
+rstconv -i units/i386-linux-gtk2/u_help.rst -o ../tools/data/language/help.po
+~/lazarus/tools/updatepofiles ../tools/data/language/help.po
