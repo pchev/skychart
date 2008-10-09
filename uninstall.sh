41631ebff5ddbdd8cb4fb65daae19a51e0c266e9
Patrick Chevalley <pch@ap-i.net>
2008-10-09 19:09:49 +0000

    install script first try

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-echo Uninstall
+destdir=$1
+
+if [ -z "$destdir" ]; then
+   export destdir=/tmp/skychart
+fi
+
+echo Uninstall skychart from $destdir
+
+for f in $(cat data.lst)
+do
+  rm $destdir/share/apps/skychart/$f
+done
+
+for f in $(cat doc.lst)
+do
+  rm $destdir/share/apps/skychart/$f
+done
+
+for f in $(cat cat.lst)
+do
+  rm $destdir/share/apps/skychart/$f
+done
+
+rmdir $destdir/share/apps/skychart
+rmdir $destdir/share/apps
+rmdir $destdir/share
+rmdir $destdir/bin
+rmdir $destdir/lib
