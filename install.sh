edb98b3a6388c3296259847b5b619efd884635e3
Patrick Chevalley <pch@ap-i.net>
2008-10-10 08:56:53 +0000

    Separate bin and data install

--- install.sh
+++ install.sh
@@ -8,23 +8,20 @@ fi
 
 echo Install skychart to $destdir
 
+install -m 755 -d $destdir
 install -m 755 -d $destdir/bin
 install -m 755 -d $destdir/lib
 install -m 755 -d $destdir/share
-install -m 755 -d $destdir/share/apps
-install -m 755 -d $destdir/share/apps/skychart
+install -m 755 -d $destdir/share/applications
+install -m 755 -d $destdir/share/doc
+install -m 755 -d $destdir/share/doc/skychart
+install -m 755 -d $destdir/share/pixmaps
 
-for f in $(cat data.lst)
-do
-  install -v -D -m 644  tools/$f $destdir/share/apps/skychart/$f
-done
+install -v -m 755 -s skychart/cdc  $destdir/bin/skychart
+install -v -m 644 -s skychart/library/getdss/libgetdss.so  $destdir/lib/libgetdss.so
+install -v -m 644 -s skychart/library/plan404/libplan404.so  $destdir/lib/libplan404.so
+install -v -m 644 system_integration/Linux/share/applications/skychart.desktop $destdir/share/applications/skychart.desktop
+install -v -m 644 system_integration/Linux/share/doc/skychart/changelog $destdir/share/doc/skychart/changelog
+install -v -m 644 system_integration/Linux/share/doc/skychart/copyright $destdir/share/doc/skychart/copyright
+install -v -m 644 system_integration/Linux/share/pixmaps/skychart.xpm $destdir/share/pixmaps/skychart.xpm
 
-for f in $(cat doc.lst)
-do
-  install -v -D -m 644  tools/$f $destdir/share/apps/skychart/$f
-done
-
-for f in $(cat cat.lst)
-do
-  install -v -D -m 644  tools/$f $destdir/share/apps/skychart/$f
-done
