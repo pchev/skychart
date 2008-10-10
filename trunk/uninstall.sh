94826531e690b147ee4a5f87f8a6eb80496a0a55
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2008-10-10 08:56:53 +0000

    Separate bin and data install

--- trunk/uninstall.sh
+++ trunk/uninstall.sh
@@ -8,23 +8,19 @@ fi
 
 echo Uninstall skychart from $destdir
 
-for f in $(cat data.lst)
-do
-  rm $destdir/share/apps/skychart/$f
-done
+rm -v $destdir/bin/skychart
+rm -v $destdir/lib/libgetdss.so
+rm -v $destdir/lib/libplan404.so
+rm -v $destdir/share/applications/skychart.desktop
+rm -v $destdir/share/doc/skychart/changelog
+rm -v $destdir/share/doc/skychart/copyright
+rm -v $destdir/share/pixmaps/skychart.xpm
 
-for f in $(cat doc.lst)
-do
-  rm $destdir/share/apps/skychart/$f
-done
-
-for f in $(cat cat.lst)
-do
-  rm $destdir/share/apps/skychart/$f
-done
-
-rmdir $destdir/share/apps/skychart
-rmdir $destdir/share/apps
-rmdir $destdir/share
-rmdir $destdir/bin
-rmdir $destdir/lib
+rmdir -v $destdir/share/doc/skychart
+rmdir -v $destdir/share/doc
+rmdir -v $destdir/share/applications
+rmdir -v $destdir/share/pixmaps
+rmdir -v $destdir/share
+rmdir -v $destdir/bin
+rmdir -v $destdir/lib
+rmdir -v $destdir
