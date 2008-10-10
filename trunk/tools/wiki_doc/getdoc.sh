66ed758f2b83cb1eb36c33b6c5232eef4e4c47b8
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2008-10-10 20:57:17 +0000

    update procedure to build the documentation from the wiki

--- trunk/tools/wiki_doc/getdoc.sh
+++ trunk/tools/wiki_doc/getdoc.sh
@@ -4,6 +4,16 @@ echo change ap-i.net to 127.0.0.1
 echo ready ?
 read
 
-wget -E -r -p -np -nH --cut-dirs=1 -X */*detail,*/*export,*/playground -R *@do=index* -P doc_en --cache=off --restrict-file-names=windows -k  http://ap-i.net/skychart/en/documentation/start http://ap-i.net/skychart/en/news/start
+wget -E -r -p -np -nH --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc_en --cache=off --restrict-file-names=windows -k  http://ap-i.net/skychart/en/documentation/start http://ap-i.net/skychart/en/news/start
 
-wget -E -r -p -np -nH --cut-dirs=1 -X */*detail,*/*export,*/playground -R *@do=index* -P doc_fr --cache=off --restrict-file-names=windows -k http://ap-i.net/skychart/fr/documentation/start http://ap-i.net/skychart/fr/nouvelles/start
+wget -E -r -p -np -nH --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc_es --cache=off --restrict-file-names=windows -k http://ap-i.net/skychart/es/documentation/start http://ap-i.net/skychart/es/nouvelles/start
+
+wget -E -r -p -np -nH --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc_fr --cache=off --restrict-file-names=windows -k http://ap-i.net/skychart/fr/documentation/start http://ap-i.net/skychart/fr/nouvelles/start
+
+wget -E -r -p -np -nH --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc_it --cache=off --restrict-file-names=windows -k http://ap-i.net/skychart/it/documentation/start http://ap-i.net/skychart/it/news/start
+
+wget -E -r -p -np -nH --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc_nl --cache=off --restrict-file-names=windows -k http://ap-i.net/skychart/nl/documentation/start http://ap-i.net/skychart/nl/nieuws/start
+
+wget -E -r -p -np -nH --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc_ru --cache=off --restrict-file-names=windows -k http://ap-i.net/skychart/ru/documentation/start http://ap-i.net/skychart/ru/nouvelles/start
+
+wget -E -r -p -np -nH --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc_uk --cache=off --restrict-file-names=windows -k http://ap-i.net/skychart/uk/documentation/start http://ap-i.net/skychart/uk/nouvelles/start
