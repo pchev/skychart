11aec389cea74e13f8309c7cd7b878098884dc1b
Patrick Chevalley <pch@ap-i.net>
2008-11-11 17:43:03 +0000

    update makefiles for cross compilation and install

---
+++ install_win.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+destdir=$1
+
+if [ -z "$destdir" ]; then
+   export destdir=/tmp/skychart
+fi
+
+echo Install skychart to $destdir
+
+install -m 755 -d $destdir
+fpc-i386-win32-strip -v -o $destdir/skychart.exe skychart/cdc.exe 
+fpc-i386-win32-strip -v -o $destdir/cdcicon.exe skychart/cdcicon.exe
+fpc-i386-win32-strip -v -o $destdir/varobs.exe varobs/varobs.exe
+fpc-i386-win32-strip -v -o $destdir/varobs_lpv_bulletin.exe varobs/varobs_lpv_bulletin.exe
+install -v -m 644 skychart/library/getdss/libgetdss.dll  $destdir/
+install -v -m 644 skychart/library/plan404/libplan404.dll  $destdir/
+
+unzip -d $destdir system_integration/Windows/data/sqlite3.zip
+unzip -d $destdir system_integration/Windows/data/plugins.zip
---
+++ uninstall_win.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+destdir=$1
+
+if [ -z "$destdir" ]; then
+   export destdir=/tmp/skychart
+fi
+
+echo Uninstall skychart from $destdir
+
+rm -fv $destdir/skychart.exe
+rm -fv $destdir/cdcicon.exe
+rm -fv $destdir/varobs.exe
+rm -fv $destdir/varobs_lpv_bulletin.exe
+rm -fv $destdir/libgetdss.dll
+rm -fv $destdir/libplan404.dll
+rm -fv $destdir/sqlite3.dll
+rm -rfv $destdir/plugins
