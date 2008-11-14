938616fb57ee4b80a4eeb7585de224969af097c1
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2008-11-14 06:57:14 +0000

    do not include deb and rpm dir to the tar

--- trunk/daily_build.sh
+++ trunk/daily_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-builddir=/tmp/skychart  # Be sur this is set!
+builddir=/tmp/skychart  # Be sure this is set to a non existent directory, it is removed after the run!
 innosetup="C:\Program Files\Inno Setup 5\ISCC.exe"  # Install under Wine from http://www.jrsoftware.org/isinfo.php
-issscript="Z:\tmp\skychart\cdcv3.iss" # Change to match builddir, Z: is defined in ~.wine 
+issscript="Z:\tmp\skychart\cdcv3.iss" # Change to match builddir, Z: is defined in ~/.wine/dosdevices
 
 if [[ -n $1 ]]; then
   configopt="fpc=$1"
@@ -14,10 +14,10 @@ fi
 wd=`pwd`
 
 # update to last revision
-svn up
+svn up --force --non-interactive --accept theirs-full
 
-# check if new revision from last run
-read lastrev <build_daily.last
+# check if new revision since last run
+read lastrev <last.build
 lang=LANG
 LANG=C
 currentrev=`svn info . | grep Revision: | sed 's/Revision: //'`
@@ -27,15 +27,13 @@ if [[ $lastrev -ne $currentrev ]]; then
 
 # delete old files
   rm skychart-linux.tar.bz2
-  rm skychart-*.deb
-  rm skychart-*.rpm
+  rm skychart*.deb
+  rm skychart*.rpm
   rm skychart-windows.zip
-  rm skychart-*.exe
+  rm skychart*.exe
   rm -rf $builddir
 
 # make Linux version
-  rsync -a --exclude=.svn system_integration/Linux/debian $builddir
-  rsync -a --exclude=.svn system_integration/Linux/rpm $builddir
   ./configure $configopt prefix=$builddir
   if [[ $? -ne 0 ]]; then exit 1;fi
   make clean
@@ -45,11 +43,16 @@ if [[ $lastrev -ne $currentrev ]]; then
   if [[ $? -ne 0 ]]; then exit 1;fi
   make install_data
   if [[ $? -ne 0 ]]; then exit 1;fi
+  # tar
   cd $builddir
   tar cvjf skychart-linux.tar.bz2 *
   if [[ $? -ne 0 ]]; then exit 1;fi
   mv skychart-linux.tar.bz2 $wd
   if [[ $? -ne 0 ]]; then exit 1;fi
+  # deb
+  cd $wd
+  rsync -a --exclude=.svn system_integration/Linux/debian $builddir
+  cd $builddir
   mv bin debian/skychart/usr/
   mv lib debian/skychart/usr/
   mv share debian/skychart/usr/
@@ -58,6 +61,9 @@ if [[ $lastrev -ne $currentrev ]]; then
   if [[ $? -ne 0 ]]; then exit 1;fi
   mv skychart*.deb $wd
   if [[ $? -ne 0 ]]; then exit 1;fi
+  # rpm
+  cd $wd
+  rsync -a --exclude=.svn system_integration/Linux/rpm $builddir
   cd $builddir
   mv debian/skychart/usr/* rpm/skychart/usr/
   cd rpm
@@ -80,11 +86,13 @@ if [[ $lastrev -ne $currentrev ]]; then
   if [[ $? -ne 0 ]]; then exit 1;fi
   make install_win_data
   if [[ $? -ne 0 ]]; then exit 1;fi
+  # zip
   cd $builddir/Data
   zip -r  skychart-windows.zip *
   if [[ $? -ne 0 ]]; then exit 1;fi
   mv skychart-windows.zip $wd
   if [[ $? -ne 0 ]]; then exit 1;fi
+  # exe
   wine "$innosetup" "$issscript"
   if [[ $? -ne 0 ]]; then exit 1;fi
   mv $builddir/skychart-*.exe $wd
@@ -93,7 +101,7 @@ if [[ $lastrev -ne $currentrev ]]; then
   rm -rf $builddir
 
 # store revision 
-  echo $currentrev > build_daily.last
+  echo $currentrev > last.build
 else
   echo Already build at revision $currentrev
 exit 1
