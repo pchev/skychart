2a273dfe7f85515183ea92ed966b1a74e9992f01
Patrick Chevalley <pch@ap-i.net>
2008-11-16 11:15:37 +0000

    Move sample client program location Remove obsolete scripts

--- skychart/scripts/bmptops.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem convert a bmp to postscript using netpbm
-rem put with all the required netpbm files in cdc\plugins\netpbm
-bmptopnm %1 | pnmtops -equalpixels -dpi=150 -rle > %2
-exit
