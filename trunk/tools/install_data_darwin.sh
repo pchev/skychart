1e270ff873744b7fa17f855986820fbe811490c8
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2009-02-04 15:06:48 +0000

    Build script for Mac OS X

---
+++ trunk/tools/install_data_darwin.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+destdir=$1
+
+if [ -z "$destdir" ]; then
+   export destdir=/tmp/skychart
+fi
+
+echo Install skychart to $destdir
+
+install -v -m 755 -d $destdir
+
+for f in $(sort dir.lst)
+do
+  install -v -m 755 -d $destdir/$f
+done
+
+for f in $(cat data.lst)
+do
+  install -v -m 644  $f $destdir/$f
+done
+
+for f in $(cat doc.lst)
+do
+  install -v -m 644  $f $destdir/$f
+done
+
+unzip -d $destdir/doc/ doc/wiki_doc.zip
+
+for f in $(cat cat.lst)
+do
+  install -v -m 644  $f $destdir/$f
+done
+
+
