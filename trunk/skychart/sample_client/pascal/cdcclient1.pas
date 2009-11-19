70144cbac8062c46e1650c5f7f6dc1d393331753
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2009-11-19 09:13:20 +0000

    launch Skychart if it is not running

--- trunk/skychart/sample_client/pascal/cdcclient1.pas
+++ trunk/skychart/sample_client/pascal/cdcclient1.pas
@@ -28,11 +28,14 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 interface
 
-uses cu_tcpclient,
+uses cu_tcpclient, IniFiles,
   SysUtils, Types, Classes, Controls, Forms,
   Dialogs, StdCtrls, ExtCtrls, LResources;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
     Edit1: TEdit;
     Edit2: TEdit;
@@ -46,15 +49,23 @@ type
     Memo1: TMemo;
     ComboBox1: TComboBox;
     Button4: TButton;
+    ConnectRetryTimer: TTimer;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
+    procedure ConnectRetryTimerTimer(Sender: TObject);
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure Combobox1KeyDown(Sender: TObject; var Key: Word;
       Shift: TShiftState);
     procedure Button4Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
   private
     { Private declarations }
+    CdCconfig,CdC,CdCDir,ServerIPaddr,ServerIPport: string;
+    CdCfound, StartCDC,Connecting: boolean;
+    ConnectRetry: integer;
+    procedure GetCdCInfo;
+    procedure OpenCDC(param:string);
   public
     { Public declarations }
     client : TClientThrd;
@@ -65,14 +76,86 @@ type
 var
   Form1: TForm1;
 
+const
+  {$ifdef linux}
+        DefaultCdC='skychart';
+        DefaultCdCconfig='~/.skychart/skychart.ini';
+  {$endif}
+  {$ifdef darwin}
+        DefaultCdC='skychart';
+        DefaultCdCconfig='~/.skychart/skychart.ini';
+  {$endif}
+  {$ifdef win32}
+        DefaultCdC='skychart.exe';
+        DefaultCdCconfig='Skychart\skychart.ini';
+  {$endif}
+
 implementation
 
+{$ifdef win32}
+ uses  Windows, ShlObj;
+{$endif}
+{$ifdef unix}
+ uses unix,baseunix;
+{$endif}
+
+
+// some useful functions
+Function Slash(nom : string) : string;
+begin
+result:=trim(nom);
+if copy(result,length(nom),1)<>PathDelim then result:=result+PathDelim;
+end;
+
+procedure ExecNoWait(cmd: string; title:string=''; hide: boolean=true);
+{$ifdef unix}
+begin
+ fpSystem(cmd+' &');
+end;
+{$endif}
+{$ifdef win32}
+var
+   bchExec: array[0..1024] of char;
+   pchEXEC: Pchar;
+   si: TStartupInfo;
+   pi: TProcessInformation;
+begin
+   pchExec := @bchExec;
+   StrPCopy(pchExec,cmd);
+   FillChar(si,sizeof(si),0);
+   FillChar(pi,sizeof(pi),0);
+   si.dwFlags:=STARTF_USESHOWWINDOW;
+   if title<>'' then si.lpTitle:=Pchar(title);
+   if hide then si.wShowWindow:=SW_SHOWMINIMIZED
+           else si.wShowWindow:=SW_SHOWNORMAL;
+   si.cb := sizeof(si);
+   try
+     CreateProcess(Nil,pchExec,Nil,Nil,false,CREATE_NEW_CONSOLE or NORMAL_PRIORITY_CLASS, Nil,Nil,si,pi);
+    except;
+    end;
+end;
+{$endif}
 
 procedure TForm1.ShowInfo(Sender: TObject; const messagetext:string);
 begin
 // process here socket status message.
-  edit3.Text:=messagetext;
-  edit3.Invalidate;
+  if connecting and (messagetext='Connection refused')and(ConnectRetry<10) then begin
+     edit3.Text:='Launching Skychart ...';
+     edit3.Invalidate;
+     inc(ConnectRetry);
+     if ConnectRetry=1 then OpenCDC('');
+     ConnectRetryTimer.Enabled:=true;
+  end
+  else begin
+     edit3.Text:=messagetext;
+     edit3.Invalidate;
+  end;
+end;
+
+procedure TForm1.ConnectRetryTimerTimer(Sender: TObject);
+begin
+  ConnectRetryTimer.Enabled:=false;
+  Button1Click(nil);
 end;
 
 procedure TForm1.ReceiveData(Sender : TObject; const data : string);
@@ -84,19 +167,18 @@ end;
 procedure TForm1.Button1Click(Sender: TObject);
 // connect button
 begin
-writeln('connect');
 if (client=nil)or(client.Terminated) then
    client:=TClientThrd.Create
    else exit;
-writeln('set');
 client.TargetHost:=edit1.Text;
 client.TargetPort:=edit2.Text;
 client.Timeout := 500;    // tcp/ip timeout [ms] also act as a delay before to send command
 client.CmdTimeout := 10;  // cdc response timeout [seconds]
 client.onShowMessage:=ShowInfo;
 client.onReceiveData:=ReceiveData;
+Connecting:=true;
+ConnectRetry:=0;
 client.Resume;
-writeln('connected');
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
@@ -131,6 +213,7 @@ begin
 if client<>nil then begin
    client.terminate;
 end;
+if StartCDC then OpenCDC('--quit');
 end;
 
 procedure TForm1.Button4Click(Sender: TObject);
@@ -151,6 +234,69 @@ if (client<>nil)and(not client.Terminated) then begin
   if resp<>msgOK then raise exception.Create('Cannot activate '+cname);
   memo1.lines.add(resp+', new chart name is '+cname);
 end;
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+  GetCdCInfo;
+  edit1.Text:=ServerIPaddr;
+  edit2.Text:=ServerIPport;
+end;
+
+procedure TForm1.GetCdCInfo;
+var
+  buf: string;
+  inif: TMemIniFile;
+{$ifdef win32}
+  buf1: string;
+  PIDL:   PItemIDList;
+  Folder: array[0..MAX_PATH] of char;
+const
+  CSIDL_APPDATA  = $001a;   // <user name>\Application Data
+  CSIDL_LOCAL_APPDATA = $001c; // <user name>\Local Settings\Applicaiton Data (non roaming)
+{$endif}
+begin
+{$ifdef unix}
+  CdCconfig  := ExpandFileName(DefaultCdCconfig);
+{$endif}
+{$ifdef win32}
+  SHGetSpecialFolderLocation(0, CSIDL_LOCAL_APPDATA, PIDL);
+  SHGetPathFromIDList(PIDL, Folder);
+  buf1 := trim(Folder);
+  SHGetSpecialFolderLocation(0, CSIDL_APPDATA, PIDL);
+  SHGetPathFromIDList(PIDL, Folder);
+  buf := trim(Folder);
+  if buf1 = '' then
+    buf1     := buf;  // old windows version
+  CdCconfig  := slash(buf1) + DefaultCdCconfig;
+{$endif}
+CdCdir := '';
+if fileexists(CdCconfig) then
+begin
+  inif := TMeminifile.Create(CdCconfig);
+  try
+    buf := inif.ReadString('main', 'AppDir', '');
+    if DirectoryExists(buf) then
+      CdCdir := buf;
+    ServerIPaddr:=inif.ReadString('main','ServerIPaddr','127.0.0.1');
+    ServerIPport:=inif.ReadString('main','ServerIPport','3292');
+
+  finally
+    inif.Free;
+  end;
+  CdC := slash(CdCdir) + DefaultCdC;
+  if not FileExists(CdC) then begin
+     CdC :=ExpandFileName(slash(CdCdir)+slash('..')+slash('..')+slash('bin') + DefaultCdC);
+  end;
+  CdCfound:=FileExists(CdC);
+end;
+end;
+
+procedure TForm1.OpenCDC(param:string);
+begin
+    param:=param+' --unique --nosplash ';
+    Execnowait(CdC+' '+param);
+    StartCDC:=true;
 end;
 
 initialization
