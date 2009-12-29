25a7560790532fed944779f4a0997f5c47b65795
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2009-12-29 21:34:16 +0000

    simpleipc fixed in fpc 2.5.1

--- trunk/skychart/component/uniqueinstance/uniqueinstance.pas
+++ trunk/skychart/component/uniqueinstance/uniqueinstance.pas
@@ -40,6 +40,9 @@ interface
 uses
 {$ifdef unix}
   process, math,
+{ TODO : can be removed if using fpc 2.5.1 or heigher  }
+// http://bugs.freepascal.org/view.php?id=15219
+// solved by fpc 2.5.1
   cdcsimpleipc,
 {$else}
   simpleipc,
