d998070fa043e7a94754f55fbcb8f2778a3d14b1
Patrick Chevalley <pch@ap-i.net>
2010-01-16 15:03:12 +0000

    compilation procedure for x86_64

---
+++ daily_build_64.sh
@@ -0,0 +1,246 @@
+#!/bin/bash 
+
+version=3.1
+
+builddir=/tmp/skychart  # Be sure this is set to a non existent directory, it is removed after the run!
+innosetup="C:\Program Files\Inno Setup 5\ISCC.exe"  # Install under Wine from http://www.jrsoftware.org/isinfo.php
+wine_build="Z:\tmp\skychart" # Change to match builddir, Z: is defined in ~/.wine/dosdevices
+
+if [[ -n $1 ]]; then
+  configopt="fpc=$1"
+fi
+if [[ -n $2 ]]; then
+  configopt=$configopt" lazarus=$2"
+fi
+
+wd=`pwd`
+
+# update to last revision
+#svn up --force --non-interactive --accept theirs-full    # svn 1.5 only
+#svn -R revert .
+#svn up --non-interactive
+
+# check if new revision since last run
+read lastrev <last.build
+lang=LANG
+LANG=C
+currentrev=`svn info . | grep Revision: | sed 's/Revision: //'`
+LANG=$lang
+echo $lastrev ' - ' $currentrev
+if [[ $lastrev -ne $currentrev ]]; then
+
+# delete old files
+  rm skychart*.bz2
+  rm skychart*.deb
+  rm skychart*.rpm
+  rm skychart*.zip
+  rm skychart*.exe
+  rm bin-*.zip
+  rm bin-*.bz2
+  rm -rf $builddir
+
+# make Linux i386 version
+  ./configure $configopt prefix=$builddir target=i386-linux,x86_64-linux
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  make CPU_TARGET=i386 OS_TARGET=linux clean
+  make CPU_TARGET=i386 OS_TARGET=linux
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  make install
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  make install_data
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  # tar
+  cd $builddir
+  tar cvjf skychart-$version-$currentrev-linux_i386.tar.bz2 *
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  mv skychart*.tar.bz2 $wd
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  # deb
+  cd $wd
+  rsync -a --exclude=.svn system_integration/Linux/debian $builddir
+  cd $builddir
+  mv bin debian/skychart/usr/
+  mv lib debian/skychart/usr/
+  mv share debian/skychart/usr/
+  cd debian
+  sed -i "/Version:/ s/3/$version-$currentrev/" skychart/DEBIAN/control
+  fakeroot dpkg-deb --build skychart .
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  mv skychart*.deb $wd
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  # rpm
+  cd $wd
+  rsync -a --exclude=.svn system_integration/Linux/rpm $builddir
+  cd $builddir
+  mv debian/skychart/usr/* rpm/skychart/usr/
+  cd rpm
+  sed -i "/Version:/ s/3/$version/"  SPECS/skychart.spec
+  sed -i "/Release:/ s/1/$currentrev/" SPECS/skychart.spec
+  setarch i386 fakeroot rpmbuild  --buildroot "$builddir/rpm/skychart" --define "_topdir $builddir/rpm/" -bb SPECS/skychart.spec
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  mv RPMS/i386/skychart*.rpm $wd
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  #debug
+  cd $wd
+  mkdir $builddir/debug
+  cp skychart/cdc $builddir/debug/skychart
+  cp skychart/cdcicon $builddir/debug/
+  cp varobs/varobs $builddir/debug/
+  cp varobs/varobs_lpv_bulletin $builddir/debug/
+  cd $builddir/debug/
+  tar cvjf bin-linux_i386-debug-$currentrev.tar.bz2 *
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  mv bin-*.tar.bz2 $wd
+  if [[ $? -ne 0 ]]; then exit 1;fi
+
+  cd $wd
+  rm -rf $builddir
+
+# make Linux x86_64 version
+  ./configure $configopt prefix=$builddir target=x86_64-linux
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  make CPU_TARGET=x86_64 OS_TARGET=linux clean
+  make CPU_TARGET=x86_64 OS_TARGET=linux
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  make install
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  make install_data
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  # tar
+  cd $builddir
+  tar cvjf skychart-$version-$currentrev-linux_x86_64.tar.bz2 *
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  mv skychart*.tar.bz2 $wd
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  # deb
+  cd $wd
+  rsync -a --exclude=.svn system_integration/Linux/debian $builddir
+  cd $builddir
+  mv bin debian/skychart64/usr/
+  mv lib debian/skychart64/usr/
+  mv share debian/skychart64/usr/
+  cd debian
+  sed -i "/Version:/ s/3/$version-$currentrev/" skychart64/DEBIAN/control
+  fakeroot dpkg-deb --build skychart64 .
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  mv skychart*.deb $wd
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  # rpm
+  cd $wd
+  rsync -a --exclude=.svn system_integration/Linux/rpm $builddir
+  cd $builddir
+  mv debian/skychart64/usr/* rpm/skychart/usr/
+  # Redhat 64bits lib is lib64 
+  mv rpm/skychart/usr/lib rpm/skychart/usr/lib64
+  cd rpm
+  sed -i "/Version:/ s/3/$version/"  SPECS/skychart64.spec
+  sed -i "/Release:/ s/1/$currentrev/" SPECS/skychart64.spec
+# rpm 4.7
+  fakeroot rpmbuild  --buildroot "$builddir/rpm/skychart" --define "_topdir $builddir/rpm/" -bb SPECS/skychart64.spec
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  mv RPMS/x86_64/skychart*.rpm $wd
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  #debug
+  cd $wd
+  mkdir $builddir/debug
+  cp skychart/cdc $builddir/debug/skychart
+  cp skychart/cdcicon $builddir/debug/
+  cp varobs/varobs $builddir/debug/
+  cp varobs/varobs_lpv_bulletin $builddir/debug/
+  cd $builddir/debug/
+  tar cvjf bin-linux_x86_64-debug-$currentrev.tar.bz2 *
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  mv bin-*.tar.bz2 $wd
+  if [[ $? -ne 0 ]]; then exit 1;fi
+
+  cd $wd
+  rm -rf $builddir
+
+# make Windows i386 version
+  rsync -a --exclude=.svn system_integration/Windows/installer/skychart/* $builddir
+  ./configure $configopt prefix=$builddir/Data target=i386-win32,x86_64-linux
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  make OS_TARGET=win32 CPU_TARGET=i386 clean
+  make OS_TARGET=win32 CPU_TARGET=i386
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  make install_win
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  make install_win_data
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  # zip
+  cd $builddir/Data
+  zip -r  skychart-$version-$currentrev-windows.zip *
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  mv skychart*.zip $wd
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  # exe
+  cd $builddir
+  sed -i "/AppVerName/ s/V3/V$version/" cdcv3.iss
+  sed -i "/OutputBaseFilename/ s/windows/$version-$currentrev-windows/" cdcv3.iss
+  wine "$innosetup" "$wine_build\cdcv3.iss"
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  mv $builddir/skychart*.exe $wd
+  #debug
+  cd $wd
+  mkdir $builddir/debug
+  cp skychart/cdc.exe $builddir/debug/skychart.exe
+  cp skychart/cdcicon.exe $builddir/debug/
+  cp varobs/varobs.exe $builddir/debug/
+  cp varobs/varobs_lpv_bulletin.exe $builddir/debug/
+  cd $builddir/debug/
+  zip bin-windows_i386-debug-$currentrev.zip *
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  mv bin-*.zip $wd
+  if [[ $? -ne 0 ]]; then exit 1;fi
+
+  cd $wd
+  rm -rf $builddir
+
+# make Windows x86_64 version
+  rsync -a --exclude=.svn system_integration/Windows/installer/skychart/* $builddir
+  ./configure $configopt prefix=$builddir/Data target=x86_64-win64,x86_64-linux
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  make OS_TARGET=win64 CPU_TARGET=x86_64 clean
+  make OS_TARGET=win64 CPU_TARGET=x86_64
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  make install_win64
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  make install_win_data
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  # zip
+  cd $builddir/Data
+  zip -r  skychart-$version-$currentrev-windows-x64.zip *
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  mv skychart*.zip $wd
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  # exe
+  cd $builddir
+  sed -i "/AppVerName/ s/V3/V$version/" cdcv3_64.iss
+  sed -i "/OutputBaseFilename/ s/windows-x64/$version-$currentrev-windows-x64/" cdcv3_64.iss
+  wine "$innosetup" "$wine_build\cdcv3_64.iss"
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  mv $builddir/skychart*.exe $wd
+  #debug
+  cd $wd
+  mkdir $builddir/debug
+  cp skychart/cdc.exe $builddir/debug/skychart.exe
+  cp skychart/cdcicon.exe $builddir/debug/
+  cp varobs/varobs.exe $builddir/debug/
+  cp varobs/varobs_lpv_bulletin.exe $builddir/debug/
+  cd $builddir/debug/
+  zip bin-windows-x64-debug-$currentrev.zip *
+  if [[ $? -ne 0 ]]; then exit 1;fi
+  mv bin-*.zip $wd
+  if [[ $? -ne 0 ]]; then exit 1;fi
+
+  cd $wd
+  rm -rf $builddir
+
+
+# store revision 
+  echo $currentrev > last.build
+else
+  echo Already build at revision $currentrev
+  exit 4
+fi
+
--- install_win.sh
+++ install_win.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
-destdir=$1
+OS_TARGET=$1
+destdir=$2
+
+if [ -z "$OS_TARGET=" ]; then
+   export OS_TARGET==win32
+fi
 
 if [ -z "$destdir" ]; then
    export destdir=/tmp/skychart
 fi
 
-echo Install skychart to $destdir
+echo Install skychart $OS_TARGET to $destdir
 
 install -m 755 -d $destdir
-fpc-i386-win32-strip -v -o $destdir/skychart.exe skychart/cdc.exe 
-fpc-i386-win32-strip -v -o $destdir/cdcicon.exe skychart/cdcicon.exe
-fpc-i386-win32-strip -v -o $destdir/varobs.exe varobs/varobs.exe
-fpc-i386-win32-strip -v -o $destdir/varobs_lpv_bulletin.exe varobs/varobs_lpv_bulletin.exe
-install -v -m 644 skychart/library/getdss/libgetdss.dll  $destdir/
-install -v -m 644 skychart/library/plan404/libplan404.dll  $destdir/
 
-unzip -d $destdir system_integration/Windows/data/sqlite3.zip
-unzip -d $destdir system_integration/Windows/data/plugins.zip
-unzip -d $destdir system_integration/Windows/data/planetrender.zip
-unzip -d $destdir system_integration/Windows/data/zlib.zip
+if [ $OS_TARGET = win32 ]; then 
+  i386-win32-strip -v -o $destdir/skychart.exe skychart/cdc.exe 
+  i386-win32-strip -v -o $destdir/cdcicon.exe skychart/cdcicon.exe
+  i386-win32-strip -v -o $destdir/varobs.exe varobs/varobs.exe
+  i386-win32-strip -v -o $destdir/varobs_lpv_bulletin.exe varobs/varobs_lpv_bulletin.exe
+  install -v -m 644 skychart/library/getdss/libgetdss.dll  $destdir/
+  install -v -m 644 skychart/library/plan404/libplan404.dll  $destdir/
+  unzip -d $destdir system_integration/Windows/data/sqlite3.zip
+  unzip -d $destdir system_integration/Windows/data/plugins.zip
+  unzip -d $destdir system_integration/Windows/data/planetrender.zip
+  unzip -d $destdir system_integration/Windows/data/zlib.zip
+fi
+if [ $OS_TARGET = win64 ]; then
+  x86_64-win64-strip -v -o $destdir/skychart.exe skychart/cdc.exe
+  x86_64-win64-strip -v -o $destdir/cdcicon.exe skychart/cdcicon.exe
+  x86_64-win64-strip -v -o $destdir/varobs.exe varobs/varobs.exe
+  x86_64-win64-strip -v -o $destdir/varobs_lpv_bulletin.exe varobs/varobs_lpv_bulletin.exe
+  install -v -m 644 skychart/library/getdss/libgetdss_x64.dll  $destdir/libgetdss.dll
+  install -v -m 644 skychart/library/plan404/libplan404_x64.dll  $destdir/libplan404.dll
+  unzip -d $destdir system_integration/Windows/data/sqlite3_x64.zip
+  unzip -d $destdir system_integration/Windows/data/zlib_x64.zip
+fi
+
