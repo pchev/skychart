9bda8fe58302b4388f44f8d41cb48a0e928ff62a
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2010-02-06 10:27:08 +0000

    Add Hungarian translation by Peter Safar

--- trunk/skychart/makepo.sh
+++ trunk/skychart/makepo.sh
@@ -6,7 +6,7 @@
 # Update first the path to your Lazarus installation and run "make" in lazarus/tools
 
 rstconv -i units/i386-linux-gtk2/u_translation.rst -o ../tools/data/language/skychart.po
-~/lazarus/tools/updatepofiles ../tools/data/language/skychart.po
+/home/compiler/lazarus/tools/updatepofiles ../tools/data/language/skychart.po
 
 rstconv -i units/i386-linux-gtk2/u_help.rst -o ../tools/data/language/help.po
-~/lazarus/tools/updatepofiles ../tools/data/language/help.po
+/home/compiler/lazarus/tools/updatepofiles ../tools/data/language/help.po
