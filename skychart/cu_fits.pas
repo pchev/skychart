558bc82187b7be6854d7717b139216de59e8c12b
Patrick Chevalley <pch@ap-i.net>
2010-08-30 16:50:07 +0000

    Put Win98 specific code back

--- skychart/cu_fits.pas
+++ skychart/cu_fits.pas
@@ -129,7 +129,7 @@ end;
 procedure TFits.SetFile(value:string);
 begin
 try
-{$ifdef mswindows}
+{$ifdef mswindows} // Win98 do not accept \\ as path delimiter
 value:=StringReplace(value,'\\','\',[rfReplaceAll]);
 {$endif}
 Fheader.valid:=false;
