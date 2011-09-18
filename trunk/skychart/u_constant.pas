eb73acdd7e4bc20067bdc03193a1feb3ff6fe6f8
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2011-09-18 18:13:14 +0000

    update release notes

--- trunk/skychart/u_constant.pas
+++ trunk/skychart/u_constant.pas
@@ -36,7 +36,7 @@ type Starcolarray =  Array [0..Maxcolor] of Tcolor; // 0:sky, 1-10:object, 11:no
      TSkycolor = array[0..7]of Tcolor;
 
 const cdcversion = 'Version 3.5';
-      cdcver     = '3.5a';
+      cdcver     = '3.5b';
       cdccpy = 'Copyright (C) 2002-2011 Patrick Chevalley';
       cdcauthors = 'Patrick Chevalley, pch@freesurf.ch'+crlf+
                    'Peter Dean,'+crlf+
