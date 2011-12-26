34aff7b44d74aa2a90fd4349c7ea1ddbaaab9f1b
Patrick Chevalley <pch@ap-i.net>
2011-12-26 11:40:34 +0000

    uninstall library

--- uninstall.sh
+++ uninstall.sh
@@ -14,6 +14,7 @@ rm -fv $destdir/bin/varobs
 rm -fv $destdir/bin/varobs_lpv_bulletin
 rm -fv $destdir/lib/libgetdss.so
 rm -fv $destdir/lib/libplan404.so
+rm -fv $destdir/lib/libcdcwcs.so
 rm -fv $destdir/share/applications/skychart.desktop
 rm -fv $destdir/share/doc/skychart/changelog
 rm -fv $destdir/share/doc/skychart/copyright
