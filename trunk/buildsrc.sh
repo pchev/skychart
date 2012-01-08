e921c2f7235b4715ccfbbacbb09b3be0f4926daa
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2012-01-08 12:40:17 +0000

    script to make full source tar

--- trunk/buildsrc.sh
+++ trunk/buildsrc.sh
@@ -10,11 +10,20 @@ repo=https://svn.origo.ethz.ch/skychart
 builddir=/tmp/skychart-src  # Be sure this is set to a non existent directory, it is removed after the run!
 verdir=skychart-$version-src
 
+wd=`pwd`
+
 mkdir -p $builddir
 cd $builddir
 
 # export sources
 svn export $repo/$pkg $verdir
+
+# revision include
+svnrev=$(LANG=C svn info $repo/$pkg |grep "Last Changed Rev:" | sed 's/Last Changed Rev: //')
+cat <<EOF > $verdir/skychart/revision.inc
+// Created by source export for version $version
+const RevisionStr = '$svnrev';
+EOF
 
 # download doc
 cd $verdir/tools
@@ -28,3 +37,8 @@ cd -
 
 # tar files
 tar cvzf $verdir.tgz $verdir
+
+mv $verdir.tgz $wd/
+
+cd $wd
+rm -rf $builddir
