32f87ee7d00c1abd3193b1844095b5d6ca73b7e3
Patrick Chevalley <pch@ap-i.net>
2012-09-27 11:24:34 +0000

    update translation

--- skychart/makepo.sh
+++ skychart/makepo.sh
@@ -2,11 +2,16 @@
 
 # Run this script to update all the translations after modification of a 
 # resource string in u_translation.pas and compilation of the program.
-
+#
+# The command updatepofiles is run two time to avoid error with comment on top of the file
+#
 # Update first the path to your Lazarus installation and run "make" in lazarus/tools
+#
 
 rstconv -i units/x86_64-linux-gtk2/u_translation.rst -o ../tools/data/language/skychart.po
 /home/compiler/lazarus/tools/updatepofiles ../tools/data/language/skychart.po
+/home/compiler/lazarus/tools/updatepofiles ../tools/data/language/skychart.po
 
 rstconv -i units/x86_64-linux-gtk2/u_help.rst -o ../tools/data/language/help.po
+/home/compiler/lazarus/tools/updatepofiles ../tools/data/language/help.po
 /home/compiler/lazarus/tools/updatepofiles ../tools/data/language/help.po
