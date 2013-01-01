6ef08d1062726d77d0273ccf36e25965ff57933d
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2013-01-01 14:13:11 +0000

    Update timezone

--- trunk/system_integration/Windows/data/mkzoneinfo.sh
+++ trunk/system_integration/Windows/data/mkzoneinfo.sh
@@ -3,17 +3,17 @@ rm -rf zoneinfo
 rm zoneinfo.zip
 mkdir zoneinfo
 cd zoneinfo
-cp -a /usr/share/zoneinfo/America .
-cp -a /usr/share/zoneinfo/Asia .
-cp -a /usr/share/zoneinfo/Europe .
-cp -a /usr/share/zoneinfo/Africa .
-cp -a /usr/share/zoneinfo/Pacific .
-cp -a /usr/share/zoneinfo/Etc .
-cp -a /usr/share/zoneinfo/Australia .
-cp -a /usr/share/zoneinfo/Indian .
-cp -a /usr/share/zoneinfo/Atlantic .
-cp -a /usr/share/zoneinfo/Antarctica .
-cp -a /usr/share/zoneinfo/Arctic .
+cp -rL /usr/share/zoneinfo/America .
+cp -rL /usr/share/zoneinfo/Asia .
+cp -rL /usr/share/zoneinfo/Europe .
+cp -rL /usr/share/zoneinfo/Africa .
+cp -rL /usr/share/zoneinfo/Pacific .
+cp -rL /usr/share/zoneinfo/Etc .
+cp -rL /usr/share/zoneinfo/Australia .
+cp -rL /usr/share/zoneinfo/Indian .
+cp -rL /usr/share/zoneinfo/Atlantic .
+cp -rL /usr/share/zoneinfo/Antarctica .
+cp -rL /usr/share/zoneinfo/Arctic .
 cp /usr/share/zoneinfo/zone.tab . 
 cd ..
 zip -r zoneinfo.zip zoneinfo
