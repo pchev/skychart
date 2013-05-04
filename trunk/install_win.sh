157629946eae743670026167a8348bb2eb44228a
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2013-05-04 13:20:30 +0000

    Fix cross-compilation of libgetdss.dll .

--- trunk/install_win.sh
+++ trunk/install_win.sh
@@ -22,7 +22,7 @@ if [ $OS_TARGET = win32 ]; then
   i386-win32-strip -v -o $destdir/varobs_lpv_bulletin.exe varobs/varobs_lpv_bulletin.exe
   i386-win32-strip -v -o $destdir/libplan404.dll skychart/library/plan404/libplan404.dll
   i386-win32-strip -v -o $destdir/libcdcwcs.dll skychart/library/wcs/libcdcwcs.dll
-  install -v -m 644 skychart/library/getdss/libgetdss.dll  $destdir/
+  i386-win32-strip -v -o $destdir/libgetdss.dll skychart/library/getdss/libgetdss.dll
   unzip -d $destdir system_integration/Windows/data/sqlite3.zip
   unzip -d $destdir system_integration/Windows/data/zlib.zip
   install -m 755 -d $destdir/data
@@ -38,6 +38,7 @@ if [ $OS_TARGET = win64 ]; then
   x86_64-win64-strip -v -o $destdir/varobs_lpv_bulletin.exe varobs/varobs_lpv_bulletin.exe
   x86_64-win64-strip -v -o $destdir/libplan404.dll skychart/library/plan404/libplan404.dll
   x86_64-win64-strip -v -o $destdir/libcdcwcs.dll skychart/library/wcs/libcdcwcs.dll
+  x86_64-win64-strip -v -o $destdir/libgetdss.dll skychart/library/getdss/libgetdss.dll
   unzip -d $destdir system_integration/Windows/data/sqlite3_x64.zip
   unzip -d $destdir system_integration/Windows/data/zlib_x64.zip
   install -m 755 -d $destdir/data
