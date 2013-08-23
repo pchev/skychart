57d6aa0cb594f03da7b83a223c68a94d515791ca
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2013-08-23 14:16:57 +0000

    Constellation figure conversion program for Lazarus.

--- trunk/tools/data/constellation/src/figures/convconstlunit1.pas
+++ trunk/tools/data/constellation/src/figures/convconstlunit1.pas
@@ -1,4 +1,7 @@
 unit convconstlUnit1;
+
+{$MODE Delphi}
+
 {
 Copyright (C) 2002 Patrick Chevalley
 
@@ -25,40 +28,75 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 interface
 
-uses libcatalog, FileCtrl, Registry,
-  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  StdCtrls, Buttons, Mask,  ComCtrls;
+uses FileCtrl, Registry,
+  LCLIntf, LCLType, LMessages, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  StdCtrls, Buttons, MaskEdit,  ComCtrls, FileUtil;
 
 type
-  TForm1 = class(TForm)
+
+  { TConvForm }
+
+  TConvForm = class(TForm)
     BitBtn1: TBitBtn;
     GroupBox1: TGroupBox;
     GroupBox2: TGroupBox;
     BitBtn2: TBitBtn;
-    GroupBox3: TGroupBox;
-    CheckBox1: TCheckBox;
-    ProgressBar1: TProgressBar;
     FilenameEdit1: TEdit;
     FilenameEdit2: TEdit;
+    Label1: TLabel;
     procedure BitBtn1Click(Sender: TObject);
     procedure BitBtn2Click(Sender: TObject);
     procedure FormShow(Sender: TObject);
   private
     { Private declarations }
+    hr: array[1..10000,1..2] of double;
+    procedure getHR;
   public
     { Public declarations }
   end;
 
 var
-  Form1: TForm1;
+  ConvForm: TConvForm;
 
 implementation
 
-{$R *.DFM}
+{$R *.lfm}
 
-VAR  BSCpath, appdir, backuppath : string;
+function copyp(t1: string; First, last: integer): string;
+begin
+  Result := copy(t1, First, last - First + 1);
+end;
 
-procedure TForm1.BitBtn1Click(Sender: TObject);
+procedure TConvForm.getHR;
+var
+  bsc: textfile;
+  buf: string;
+  ar,de,des : double;
+  n: integer;
+begin
+  AssignFile(bsc, 'bsc_catalog.dat');
+  reset(bsc);
+  n:=0;
+  while not EOF(bsc) do
+  begin
+    readln(bsc, buf);
+    inc(n);
+    ar := strtofloatdef(trim(copyp(buf, 76, 77)),0)+
+          strtofloatdef(trim(copyp(buf, 78, 79)),0)/60+
+          strtofloatdef(trim(copyp(buf, 80, 83)),0)/3600;
+    des:= strtofloatdef(trim(copyp(buf, 84, 84))+'1',1);
+    de := strtofloatdef(trim(copyp(buf, 85, 86)),0)+
+          strtofloatdef(trim(copyp(buf, 87, 88)),0)/60+
+          strtofloatdef(trim(copyp(buf, 89, 90)),0)/3600;
+    de := de * des;
+    hr[n,1]:=ar;
+    hr[n,2]:=de;
+  end;
+  closefile(bsc);
+end;
+
+
+procedure TConvForm.BitBtn1Click(Sender: TObject);
 type tconstl = record ar1,de1,ar2,de2 : double; end;
 var fi : textfile;
     fo : textfile;
@@ -69,31 +107,16 @@ var fi : textfile;
     ok : boolean;
     msg : array[0..99]of char;
 begin
-chdir(appdir);
-if checkbox1.checked then begin
-   forcedirectories(backuppath);
-   buf:=backuppath+extractfilename(filenameedit2.text)+'.'+trim(formatdatetime('yyyymmddhhnnss',now));
-   if not copyfile(pchar(filenameedit2.text),pchar(buf),false) then begin
-      H:=getlasterror;
-      formatmessage(FORMAT_MESSAGE_FROM_SYSTEM,nil,H,0,addr(msg[0]),99,nil);
-      showmessage('Backup file :'+chr(10)+chr(13)+msg);
-      exit;
-   end;
-end;
+label1.Caption:='';
+Application.ProcessMessages;
 assignfile(fi,filenameedit1.text);
 reset(fi);
 assignfile(fo,filenameedit2.text);
 rewrite(fo);
-ProgressBar1.min:=0;
-ProgressBar1.max:=filesize(fi);
-ProgressBar1.position:=0;
 try
-bitbtn1.enabled:=false;
-bitbtn2.enabled:=false;
-setbscpath(bscpath);
+getHR;
 repeat
   readln(fi,buf);
-  ProgressBar1.position:=filepos(fi);
   application.processmessages;
   if trim(buf)='' then continue;
   if copy(buf,1,1)=';' then continue;
@@ -101,44 +124,33 @@ repeat
   h1:=trim(copy(buf,6,5));
   h2:=trim(copy(buf,13,5));
   h:= strtoint(h1);
-  findnumhr(h,ar,de,ok);
-  if not ok then raise exception.create('Star HR'+h1+' not found. Line in error : '+buf);
-  r.ar1:=ar;
-  r.de1:=de;
+  r.ar1:=hr[h,1];
+  r.de1:=hr[h,2];
   h:= strtoint(h2);
-  findnumhr(h,ar,de,ok);
-  if not ok then raise exception.create('Star HR'+h2+' not found. Line in error : '+buf);
-  r.ar2:=ar;
-  r.de2:=de;
+  r.ar2:=hr[h,1];
+  r.de2:=hr[h,2];
   writeln(fo,format('%8.5f %8.4f %8.5f %8.4f',[r.ar1,r.de1,r.ar2,r.de2]));
 until eof(fi);
 closefile(fi);
 closefile(fo);
-bitbtn1.enabled:=true;
-bitbtn2.enabled:=true;
-form1.close;
+label1.Caption:='Completed!';
 except
-closefile(fi);
-closefile(fo);
-bitbtn1.enabled:=true;
-bitbtn2.enabled:=true;
-raise;
+ closefile(fi);
+ closefile(fo);
+ raise;
 end
 end;
 
-procedure TForm1.BitBtn2Click(Sender: TObject);
+procedure TConvForm.BitBtn2Click(Sender: TObject);
 begin
 Close;
 end;
 
-procedure TForm1.FormShow(Sender: TObject);
+procedure TConvForm.FormShow(Sender: TObject);
 begin
-AppDir:=trim(GetCurrentDir);
-bscpath:='cat\bsc5';
-if copy(AppDir,length(Appdir),1)='\' then Appdir:=copy(AppDir,1,length(Appdir)-1);
-backuppath:=expandfilename(bscpath+'\backup\');
-filenameedit1.Text:=bscpath+'\DefaultConstL.txt';
-filenameedit2.Text:=bscpath+'\ConstL.dat';
+label1.Caption:='';
+filenameedit1.Text:='DefaultConstL.txt';
+filenameedit2.Text:='DefaultConstL.cln';
 end;
 
 end.
