26b3fb2cf58bb88475c2bbf36754cad708be7165
Patrick Chevalley <pch@ap-i.net>
2013-09-29 16:20:08 +0000

    Patch by Mattia Verga to add appdata file

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ install -m 755 -d $destdir
 install -m 755 -d $destdir/bin
 install -m 755 -d $destdir/lib
 install -m 755 -d $destdir/share
+install -m 755 -d $destdir/share/appdata
 install -m 755 -d $destdir/share/applications
 install -m 755 -d $destdir/share/doc
 install -m 755 -d $destdir/share/doc/skychart
@@ -33,6 +34,7 @@ install -v -m 644 -s skychart/library/getdss/libgetdss.so  $destdir/lib/libgetds
 install -v -m 644 -s skychart/library/plan404/libplan404.so  $destdir/lib/libplan404.so
 install -v -m 644 -s skychart/library/wcs/libcdcwcs.so  $destdir/lib/libcdcwcs.so
 install -v -m 644 system_integration/Linux/share/applications/skychart.desktop $destdir/share/applications/skychart.desktop
+install -v -m 644 system_integration/Linux/share/appdata/skychart.appdata.xml $destdir/share/appdata/skychart.appdata.xml
 install -v -m 644 system_integration/Linux/share/doc/skychart/changelog $destdir/share/doc/skychart/changelog
 install -v -m 644 system_integration/Linux/share/doc/skychart/copyright $destdir/share/doc/skychart/copyright
 install -v -m 644 system_integration/Linux/share/pixmaps/skychart.png $destdir/share/pixmaps/skychart.png
