3667e5fd589fc03b62c1ec895b81c6504ccd251f
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2013-10-14 17:27:33 +0000

    New computation for Uranus satellites.

--- trunk/skychart/cu_plansat.pas
+++ trunk/skychart/cu_plansat.pas
@@ -37,6 +37,7 @@ double20 = array[1..20] of double;
 function MarSatAll(jde: double; var xsat,ysat,zsat : double20):integer;
 function JupSatAll(jde: double; var xsat,ysat,zsat : double20):integer;
 function SatSatAll(jde: double; var xsat,ysat,zsat : double20):integer;
+function UraSatAll(jde: double; var xsat,ysat,zsat : double20):integer;
 
 implementation
 
@@ -59,6 +60,7 @@ Tbody =(
     );
 
 const deg_to_rad = pi/180.0;
+      TWO_PI = 2*pi;
       AU_to_km = 149597870.66;
 
 procedure rotateX(var X, Y, Z: double; theta:double);
@@ -118,6 +120,46 @@ begin
         result += delta;
     end;
 end;
+
+function solveKepler(L,K,H: double): double;
+var F,E,F0,E0,eps,SF,CF,FF0,FPF0,SDIR,denom : double;
+    i : integer;
+begin
+    if (L = 0) then begin
+      result:=0;
+      exit;
+    end;
+
+    F := L;
+    F0 := L;
+    E0 := abs(L);
+
+    eps := 1e-16;
+    for i := 0 to 19 do begin
+        SF := sin(F0);
+        CF := cos(F0);
+        FF0 := F0 - K*SF + H*CF - L;
+        FPF0 := 1 - K*CF - H*SF;
+        SDIR := FF0/FPF0;
+
+        denom := 1;
+        while true do begin
+            F := F0 - SDIR / denom;
+            E := abs(F-F0);
+            if (E <= E0) then break;
+            denom *= 2;
+        end;
+
+        if (denom = 1)and(E <= eps)and(FF0 <= eps) then begin
+          result:=F;
+          exit;
+        end;
+        F0 := F;
+        E0 := E;
+    end;
+    result:=F;
+end;
+
 
 function marsat(jd:double; b:Tbody; var X,Y,Z: double):boolean;
 {
@@ -1079,20 +1121,483 @@ begin
         // rotate to earth equator J2000
         eps := 23.4392911 * deg_to_rad;
         rotateX(X, Y, Z, -eps);
-{
-        const double VX1 = vx1 * rtp + vy1 * rdg;
-        const double VY1 = vx1 * rdg + vy1 * rtq;
-        const double VZ1 = (-vx1 * elem[5] + vy1 * elem[4]) * dwho;
 
-        Vx = co * VX1 - so * ci * VY1 + so * si * VZ1;
-        Vy = so * VX1 + co * ci * VY1 - co * si * VZ1;
-        Vz = si * VY1 + ci * VZ1;
-}
     end;
 end;
 
 
+procedure UracalcRectangular(N,L,K,H,Q,P,GMS: double; var X,Y,Z: double);
+var A,PHI,PSI,RKI,F,SF,CF,RLMF: double;
+    rot: array[0..2,0..1] of double;
+    TX : array [0..1] of double;
+begin
+    // Calculate the semi-major axis
+    A := power(GMS/(N*N), 1.0/3.0) / AU_to_km;
 
+    PHI := sqrt(1 - K*K - H*H);
+    PSI := 1/(1+PHI);
+
+    RKI := sqrt(1 - Q*Q - P*P);
+
+    F := solveKepler(L, K, H);
+
+    SF := sin(F);
+    CF := cos(F);
+
+    RLMF := -K*SF + H*CF;
+
+    rot[0][0] := 1 - 2*P*P;
+    rot[0][1] := 2*P*Q;
+    rot[1][0] := 2*P*Q;
+    rot[1][1] := 1 - 2*Q*Q;
+    rot[2][0] := -2*P*RKI;
+    rot[2][1] := 2*Q*RKI;
+
+    TX[0] := A*(CF - PSI * H * RLMF - K);
+    TX[1] := A*(SF + PSI * K * RLMF - H);
+
+    X := rot[0][0] * TX[0] + rot[0][1] * TX[1];
+    Y := rot[1][0] * TX[0] + rot[1][1] * TX[1];
+    Z := rot[2][0] * TX[0] + rot[2][1] * TX[1];
+end;
+
+procedure UranicentricToGeocentricEquatorial(var X,Y,Z : double);
+// convert UME50* coordinates to EME50
+var alpha0,delta0,sa,sd,ca,cd,oldX,oldY,oldZ: double;
+begin
+    alpha0 := 76.6067 * deg_to_rad;
+    delta0 := 15.0322 * deg_to_rad;
+
+    sa := sin(alpha0);
+    sd := sin(delta0);
+    ca := cos(alpha0);
+    cd := cos(delta0);
+
+    oldX := X;
+    oldY := Y;
+    oldZ := Z;
+
+    X :=  sa * oldX + ca * sd * oldY + ca * cd * oldZ;
+    Y := -ca * oldX + sa * sd * oldY + sa * cd * oldZ;
+    Z := -cd * oldY + sd * oldZ;
+end;
+
+function urasat(jd:double; b:Tbody; var X,Y,Z: double):boolean;
+{
+  GUST86 ephemeris is described in Laskar & Jacobson,
+  Astron. Astrophys. 188, 212-224 (1987)
+
+  Much of this code is translated from the GUST86 FORTRAN code which
+  is at ftp://ftp.bdl.fr/pub/ephem/satel/gust86
+}
+var t,tcen,N1,N2,N3,N4,N5,E1,E2,E3,E4,E5: double;
+    I1,I2,I3,I4,I5,GM1,GM2,GM3,GM4,GM5,GMU,N,L,K,H,Q,P,GMS: double;
+begin
+    t := jd - 2444239.5;
+    tcen := t/365.25;
+
+    N1 := rmod(4.445190550 * t - 0.238051, TWO_PI);
+    N2 := rmod(2.492952519 * t + 3.098046, TWO_PI);
+    N3 := rmod(1.516148111 * t + 2.285402, TWO_PI);
+    N4 := rmod(0.721718509 * t + 0.856359, TWO_PI);
+    N5 := rmod(0.466692120 * t - 0.915592, TWO_PI);
+
+    E1 := (20.082 * deg_to_rad * tcen + 0.611392);
+    E2 := ( 6.217 * deg_to_rad * tcen + 2.408974);
+    E3 := ( 2.865 * deg_to_rad * tcen + 2.067774);
+    E4 := ( 2.078 * deg_to_rad * tcen + 0.735131);
+    E5 := ( 0.386 * deg_to_rad * tcen + 0.426767);
+
+    I1 := (-20.309 * deg_to_rad * tcen + 5.702313);
+    I2 := ( -6.288 * deg_to_rad * tcen + 0.395757);
+    I3 := ( -2.836 * deg_to_rad * tcen + 0.589326);
+    I4 := ( -1.843 * deg_to_rad * tcen + 1.746237);
+    I5 := ( -0.259 * deg_to_rad * tcen + 4.206896);
+
+    GM1 := 4.4;
+    GM2 := 86.1;
+    GM3 := 84.0;
+    GM4 := 230.0;
+    GM5 := 200.0;
+    GMU := 5794554.5 - (GM1 + GM2 + GM3 + GM4 + GM5);
+
+    N := 0; L := 0; K := 0; H := 0; Q := 0; P := 0; GMS := 0;
+
+    case b of
+    MIRANDA: begin
+        N := (4443522.67
+             - 34.92 * cos(N1 - 3*N2 + 2*N3)
+             +  8.47 * cos(2*N1 - 6*N2 + 4*N3)
+             +  1.31 * cos(3*N1 - 9*N2 + 6*N3)
+             - 52.28 * cos(N1 - N2)
+             -136.65 * cos(2*N1 - 2*N2)) * 1e-6;
+
+        L := (-238051.58
+             + 4445190.55 * t
+             + 25472.17 * sin(N1 - 3*N2 + 2*N3)
+             -  3088.31 * sin(2*N1 - 6*N2 + 4*N3)
+             -   318.10 * sin(3*N1 - 9*N2 + 6*N3)
+             -    37.49 * sin(4*N1 - 12*N2 + 8*N3)
+             -    57.85 * sin(N1 - N2)
+             -    62.32 * sin(2*N1 - 2*N2)
+             -    27.95 * sin(3*N1 - 3*N2)) * 1e-6;
+
+        K := (1312.38 * cos(E1)
+             + 71.81 * cos(E2)
+             + 69.77 * cos(E3)
+             +  6.75 * cos(E4)
+             +  6.27 * cos(E5)
+             - 123.31 * cos(-N1 + 2*N2)
+             +  39.52 * cos(-2*N1 + 3*N2)
+             + 194.10 * cos(N1)) * 1e-6;
+
+        H := (1312.38 * sin(E1)
+             + 71.81 * sin(E2)
+             + 69.77 * sin(E3)
+             +  6.75 * sin(E4)
+             +  6.27 * sin(E5)
+             - 123.31 * sin(-N1 + 2*N2)
+             +  39.52 * sin(-2*N1 + 3*N2)
+             + 194.10 * sin(N1)) * 1e-6;
+
+        Q := (37871.71 * cos(I1)
+             +  27.01 * cos(I2)
+             +  30.76 * cos(I3)
+             +  12.18 * cos(I4)
+             +   5.37 * cos(I5)) * 1e-6;
+
+        P := (37871.71 * sin(I1)
+             +  27.01 * sin(I2)
+             +  30.76 * sin(I3)
+             +  12.18 * sin(I4)
+             +   5.37 * sin(I5)) * 1e-6;
+
+        GMS := GMU + GM1;
+        result:=true;
+    end;
+    ARIEL: begin
+        N := (2492542.57
+             +   2.55 * cos(N1 - 3*N2 + 2*N3)
+             -  42.16 * cos(N2 - N3)
+             - 102.56 * cos(2*N2 - 2*N3)) * 1e-6;
+
+        L := (3098046.41
+             + 2492952.52 * t
+             - 1860.50 * sin(N1 - 3*N2 + 2*N3)
+             +  219.99 * sin(2*N1 - 6*N2 + 4*N3)
+             +   23.10 * sin(3*N1 - 9*N2 + 6*N3)
+             +    4.30 * sin(4*N1 - 12*N2 + 8*N3)
+             -   90.11 * sin(N2 - N3)
+             -   91.07 * sin(2*(N2 - N3))
+             -   42.75 * sin(3*(N2 - N3))
+             -   16.49 * sin(2*(N2 - N4))) * 1e-6;
+
+        K := (-    3.35 * cos(E1)
+             + 1187.63 * cos(E2)
+             +  861.59 * cos(E3)
+             +   71.50 * cos(E4)
+             +   55.59 * cos(E5)
+             -   84.60 * cos(-N2 + 2*N3)
+             +   91.81 * cos(-2*N2 + 3*N3)
+             +   20.03 * cos(-N2 + 2*N4)
+             +   89.77 * cos(N2)) * 1e-6;
+
+        H := (-    3.35 * sin(E1)
+             + 1187.63 * sin(E2)
+             +  861.59 * sin(E3)
+             +   71.50 * sin(E4)
+             +   55.59 * sin(E5)
+             -   84.60 * sin(-N2 + 2*N3)
+             +   91.81 * sin(-2*N2 + 3*N3)
+             +   20.03 * sin(-N2 + 2*N4)
+             +   89.77 * sin(N2)) * 1e-6;
+
+        Q := (- 121.75 * cos(I1)
+             + 358.25 * cos(I2)
+             + 290.08 * cos(I3)
+             +  97.78 * cos(I4)
+             +  33.97 * cos(I5)) * 1e-6;
+
+        P := (- 121.75 * sin(I1)
+             + 358.25 * sin(I2)
+             + 290.08 * sin(I3)
+             +  97.78 * sin(I4)
+             +  33.97 * sin(I5)) * 1e-6;
+
+        GMS := GMU + GM2;
+        result:=true;
+    end;
+    UMBRIEL: begin
+        N := (1515954.90
+             +   9.74 * cos(N3 - 2*N4 + E3)
+             - 106.00 * cos(N2 - N3)
+             +  54.16 * cos(2*(N2 - N3))
+             -  23.59 * cos(N3 - N4)
+             -  70.70 * cos(2*(N3 - N4))
+             -  36.28 * cos(3*(N3 - N4))) * 1e-6;
+
+        L := (2285401.69
+             + 1516148.11 * t
+             + 660.57 * sin(  N1 - 3*N2 + 2*N3)
+             -  76.51 * sin(2*N1 - 6*N2 + 4*N3)
+             -   8.96 * sin(3*N1 - 9*N2 + 6*N3)
+             -   2.53 * sin(4*N1 - 12*N2 + 8*N3)
+             -  52.91 * sin(N3 - 4*N4 + 3*N5)
+             -   7.34 * sin(N3 - 2*N4 + E5)
+             -   1.83 * sin(N3 - 2*N4 + E4)
+             + 147.91 * sin(N3 - 2*N4 + E3)
+             -   7.77 * sin(N3 - 2*N4 + E2)
+             +  97.76 * sin(N2 - N3)
+             +  73.13 * sin(2*(N2 - N3))
+             +  34.71 * sin(3*(N2 - N3))
+             +  18.89 * sin(4*(N2 - N3))
+             -  67.89 * sin(N3 - N4)
+             -  82.86 * sin(2*(N3 - N4))
+             -  33.81 * sin(3*(N3 - N4))
+             -  15.79 * sin(4*(N3 - N4))
+             -  10.21 * sin(N3 - N5)
+             -  17.08 * sin(2*(N3 - N5))) * 1e-6;
+
+        K := (-    0.21 * cos(E1)
+             -  227.95 * cos(E2)
+             + 3904.69 * cos(E3)
+             +  309.17 * cos(E4)
+             +  221.92 * cos(E5)
+             +   29.34 * cos(N2)
+             +   26.20 * cos(N3)
+             +   51.19 * cos(-N2+2*N3)
+             -  103.86 * cos(-2*N2+3*N3)
+             -   27.16 * cos(-3*N2+4*N3)
+             -   16.22 * cos(N4)
+             +  549.23 * cos(-N3 + 2*N4)
+             +   34.70 * cos(-2*N3 + 3*N4)
+             +   12.81 * cos(-3*N3 + 4*N4)
+             +   21.81 * cos(-N3 + 2*N5)
+             +   46.25 * cos(N3)) * 1e-6;
+
+        H := (-    0.21 * sin(E1)
+             -  227.95 * sin(E2)
+             + 3904.69 * sin(E3)
+             +  309.17 * sin(E4)
+             +  221.92 * sin(E5)
+             +   29.34 * sin(N2)
+             +   26.20 * sin(N3)
+             +   51.19 * sin(-N2+2*N3)
+             -  103.86 * sin(-2*N2+3*N3)
+             -   27.16 * sin(-3*N2+4*N3)
+             -   16.22 * sin(N4)
+             +  549.23 * sin(-N3 + 2*N4)
+             +   34.70 * sin(-2*N3 + 3*N4)
+             +   12.81 * sin(-3*N3 + 4*N4)
+             +   21.81 * sin(-N3 + 2*N5)
+             +   46.25 * sin(N3)) * 1e-6;
+
+        Q := (-   10.86 * cos(I1)
+             -   81.51 * cos(I2)
+             + 1113.36 * cos(I3)
+             +  350.14 * cos(I4)
+             +  106.50 * cos(I5)) * 1e-6;
+
+        P := (-   10.86 * sin(I1)
+             -   81.51 * sin(I2)
+             + 1113.36 * sin(I3)
+             +  350.14 * sin(I4)
+             +  106.50 * sin(I5)) * 1e-6;
+
+        GMS := GMU + GM3;
+        result:=true;
+    end;
+    TITANIA: begin
+        N := (721663.16
+             -  2.64 * cos(N3 - 2*N4 + E3)
+             -  2.16 * cos(2*N4 - 3*N5 + E5)
+             +  6.45 * cos(2*N4 - 3*N5 + E4)
+             -  1.11 * cos(2*N4 - 3*N5 + E3)
+             - 62.23 * cos(N2 - N4)
+             - 56.13 * cos(N3 - N4)
+             - 39.94 * cos(N4 - N5)
+             - 91.85 * cos(2*(N4 - N5))
+             - 58.31 * cos(3*(N4 - N5))
+             - 38.60 * cos(4*(N4 - N5))
+             - 26.18 * cos(5*(N4 - N5))
+             - 18.06 * cos(6*(N4 - N5))) * 1e-6;
+
+        L := (856358.79
+             + 721718.51 * t
+             +  20.61 * sin(N3 - 4*N4 + 3*N5)
+             -   2.07 * sin(N3 - 2*N4 + E5)
+             -   2.88 * sin(N3 - 2*N4 + E4)
+             -  40.79 * sin(N3 - 2*N4 + E3)
+             +   2.11 * sin(N3 - 2*N4 + E2)
+             -  51.83 * sin(2*N4 - 3*N5 + E5)
+             + 159.87 * sin(2*N4 - 3*N5 + E4)
+             -  35.05 * sin(2*N4 - 3*N5 + E3)
+             -   1.56 * sin(3*N4 - 4*N5 + E5)
+             +  40.54 * sin(N2 - N4)
+             +  46.17 * sin(N3 - N4)
+             - 317.76 * sin(N4 - N5)
+             - 305.59 * sin(2*(N4 - N5))
+             - 148.36 * sin(3*(N4 - N5))
+             -  82.92 * sin(4*(N4 - N5))
+             -  49.98 * sin(5*(N4 - N5))
+             -  31.56 * sin(6*(N4 - N5))
+             -  20.56 * sin(7*(N4 - N5))
+             -  13.69 * sin(8*(N4 - N5))) * 1e-6;
+
+        K := (-    0.02 * cos(E1)
+             -    1.29 * cos(E2)
+             -  324.51 * cos(E3)
+             +  932.81 * cos(E4)
+             + 1120.89 * cos(E5)
+             +   33.86 * cos(N2)
+             +   17.46 * cos(N4)
+             +   16.58 * cos(-N2 + 2*N4)
+             +   28.89 * cos(N3)
+             -   35.86 * cos(-N3 + 2*N4)
+             -   17.86 * cos(N4)
+             -   32.10 * cos(N5)
+             -  177.83 * cos(-N4 + 2*N5)
+             +  793.43 * cos(-2*N4 + 3*N5)
+             +   99.48 * cos(-3*N4 + 4*N5)
+             +   44.83 * cos(-4*N4 + 5*N5)
+             +   25.13 * cos(-5*N4 + 6*N5)
+             +   15.43 * cos(-6*N4 + 7*N5)) * 1e-6;
+
+        H := (-    0.02 * sin(E1)
+             -    1.29 * sin(E2)
+             -  324.51 * sin(E3)
+             +  932.81 * sin(E4)
+             + 1120.89 * sin(E5)
+             +   33.86 * sin(N2)
+             +   17.46 * sin(N4)
+             +   16.58 * sin(-N2 + 2*N4)
+             +   28.89 * sin(N3)
+             -   35.86 * sin(-N3 + 2*N4)
+             -   17.86 * sin(N4)
+             -   32.10 * sin(N5)
+             -  177.83 * sin(-N4 + 2*N5)
+             +  793.43 * sin(-2*N4 + 3*N5)
+             +   99.48 * sin(-3*N4 + 4*N5)
+             +   44.83 * sin(-4*N4 + 5*N5)
+             +   25.13 * sin(-5*N4 + 6*N5)
+             +   15.43 * sin(-6*N4 + 7*N5)) * 1e-6;
+
+        Q := (-   1.43 * cos(I1)
+             -   1.06 * cos(I2)
+             - 140.13 * cos(I3)
+             + 685.72 * cos(I4)
+             + 378.32 * cos(I5)) * 1e-6;
+
+        P := (-   1.43 * sin(I1)
+             -   1.06 * sin(I2)
+             - 140.13 * sin(I3)
+             + 685.72 * sin(I4)
+             + 378.32 * sin(I5)) * 1e-6;
+
+        GMS := GMU + GM4;
+        result:=true;
+    end;
+    OBERON: begin
+        N := (466580.54
+             +  2.08 * cos(2*N4 - 3*N5 + E5)
+             -  6.22 * cos(2*N4 - 3*N5 + E4)
+             +  1.07 * cos(2*N4 - 3*N5 + E3)
+             - 43.10 * cos(N2 - N5)
+             - 38.94 * cos(N3 - N5)
+             - 80.11 * cos(N4 - N5)
+             + 59.06 * cos(2*(N4 - N5))
+             + 37.49 * cos(3*(N4 - N5))
+             + 24.82 * cos(4*(N4 - N5))
+             + 16.84 * cos(5*(N4 - N5))) * 1e-6;
+
+        L := (-915591.80
+             + 466692.12 * t
+             -   7.82 * sin(N3 - 4*N4 + 3*N5)
+             +  51.29 * sin(2*N4 - 3*N5 + E5)
+             - 158.24 * sin(2*N4 - 3*N5 + E4)
+             +  34.51 * sin(2*N4 - 3*N5 + E3)
+             +  47.51 * sin(N2 - N5)
+             +  38.96 * sin(N3 - N5)
+             + 359.73 * sin(N4 - N5)
+             + 282.78 * sin(2*(N4 - N5))
+             + 138.60 * sin(3*(N4 - N5))
+             +  78.03 * sin(4*(N4 - N5))
+             +  47.29 * sin(5*(N4 - N5))
+             +  30.00 * sin(6*(N4 - N5))
+             +  19.62 * sin(7*(N4 - N5))
+             +  13.11 * sin(8*(N4 - N5))) * 1e-6;
+
+        K := (        0 * cos(E1)
+             -    0.35 * cos(E2)
+             +   74.53 * cos(E3)
+             -  758.68 * cos(E4)
+             + 1397.34 * cos(E5)
+             +   39.00 * cos(N2)
+             +   17.66 * cos(-N2 + 2*N5)
+             +   32.42 * cos(N3)
+             +   79.75 * cos(N4)
+             +   75.66 * cos(N5)
+             +  134.04 * cos(-N4 + 2*N5)
+             -  987.26 * cos(-2*N4 + 3*N5)
+             -  126.09 * cos(-3*N4 + 4*N5)
+             -   57.42 * cos(-4*N4 + 5*N5)
+             -   32.41 * cos(-5*N4 + 6*N5)
+             -   19.99 * cos(-6*N4 + 7*N5)
+             -   12.94 * cos(-7*N4 + 8*N5)) * 1e-6;
+
+        H := (0 * sin(E1)
+             -    0.35 * sin(E2)
+             +   74.53 * sin(E3)
+             -  758.68 * sin(E4)
+             + 1397.34 * sin(E5)
+             +   39.00 * sin(N2)
+             +   17.66 * sin(-N2 + 2*N5)
+             +   32.42 * sin(N3)
+             +   79.75 * sin(N4)
+             +   75.66 * sin(N5)
+             +  134.04 * sin(-N4 + 2*N5)
+             -  987.26 * sin(-2*N4 + 3*N5)
+             -  126.09 * sin(-3*N4 + 4*N5)
+             -   57.42 * sin(-4*N4 + 5*N5)
+             -   32.41 * sin(-5*N4 + 6*N5)
+             -   19.99 * sin(-6*N4 + 7*N5)
+             -   12.94 * sin(-7*N4 + 8*N5)) * 1e-6;
+
+        Q := (-   0.44 * cos(I1)
+             -   0.31 * cos(I2)
+             +  36.89 * cos(I3)
+             - 596.33 * cos(I4)
+             + 451.69 * cos(I5)) * 1e-6;
+
+        P := (-   0.44 * sin(I1)
+             -   0.31 * sin(I2)
+             +  36.89 * sin(I3)
+             - 596.33 * sin(I4)
+             + 451.69 * sin(I5)) * 1e-6;
+
+        GMS := GMU + GM5;
+        result:=true;
+    end;
+    else
+        result:=false;
+    end;
+
+    if result then begin
+      X := 0;
+      Y := 0;
+      Z := 0;
+
+      N /= 86400;
+      L := rmod(L, TWO_PI);
+
+      UracalcRectangular(N, L, K, H, Q, P, GMS, X, Y, Z);
+
+      UranicentricToGeocentricEquatorial(X, Y, Z);
+
+      // precess to J2000
+      precessB1950J2000(X, Y, Z);
+    end;
+end;
 
 function MarSatAll(jde: double; var xsat,ysat,zsat : double20):integer;
 var i: integer;
@@ -1131,6 +1636,21 @@ begin
 result:=0;
 for i:=1 to 9 do begin
   if satsat(jde, tbody(ord(SATURN)+i), X,Y,Z) then begin
+    xsat[i]:=X;
+    ysat[i]:=Y;
+    zsat[i]:=Z;
+  end
+  else result:=1;
+end;
+end;
+
+function UraSatAll(jde: double; var xsat,ysat,zsat : double20):integer;
+var i: integer;
+    X,Y,Z: double;
+begin
+result:=0;
+for i:=1 to 5 do begin
+  if urasat(jde, tbody(ord(URANUS)+i), X,Y,Z) then begin
     xsat[i]:=X;
     ysat[i]:=Y;
     zsat[i]:=Z;
