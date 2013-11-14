42e21d5e146cc9f827375da12bc8440c7789be3f
Patrick Chevalley <pch@ap-i.net>
2013-11-14 15:20:07 +0000

    documentation translation change

--- tools/wiki_doc/getdoc.sh
+++ tools/wiki_doc/getdoc.sh
@@ -8,11 +8,11 @@ wget -E -r -p -np -nH --cut-dirs=2 -X */*detail,*/*export,*/playground -R *do=in
 
 wget -E -r -p -np -nH --cut-dirs=2 -X */*detail,*/*export,*/playground -R *do=index* -P doc_es --cache=off --restrict-file-names=windows -k http://ap-i.net/static/skychart/es/documentation/start http://ap-i.net/static/skychart/es/news/start
 
-wget -E -r -p -np -nH --cut-dirs=2 -X */*detail,*/*export,*/playground -R *do=index* -P doc_fr --cache=off --restrict-file-names=windows -k http://ap-i.net/static/skychart/fr/documentation/start http://ap-i.net/static/skychart/fr/nouvelles/start
+wget -E -r -p -np -nH --cut-dirs=2 -X */*detail,*/*export,*/playground -R *do=index* -P doc_fr --cache=off --restrict-file-names=windows -k http://ap-i.net/static/skychart/fr/documentation/start http://ap-i.net/static/skychart/fr/news/start
 
 wget -E -r -p -np -nH --cut-dirs=2 -X */*detail,*/*export,*/playground -R *do=index* -P doc_it --cache=off --restrict-file-names=windows -k http://ap-i.net/static/skychart/it/documentation/start http://ap-i.net/static/skychart/it/news/start
 
-wget -E -r -p -np -nH --cut-dirs=2 -X */*detail,*/*export,*/playground -R *do=index* -P doc_nl --cache=off --restrict-file-names=windows -k http://ap-i.net/static/skychart/nl/documentatie/start http://ap-i.net/static/skychart/nl/nieuws/start
+wget -E -r -p -np -nH --cut-dirs=2 -X */*detail,*/*export,*/playground -R *do=index* -P doc_nl --cache=off --restrict-file-names=windows -k http://ap-i.net/static/skychart/nl/documentation/start http://ap-i.net/static/skychart/nl/news/start
 
 wget -E -r -p -np -nH --cut-dirs=2 -X */*detail,*/*export,*/playground -R *do=index* -P doc_ru --cache=off --restrict-file-names=windows -k http://ap-i.net/static/skychart/ru/documentation/start http://ap-i.net/static/skychart/ru/news/start
 
