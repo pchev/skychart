pchev/skychart

commit f10eea3656bc0a856114ae509804a9564b38623a

Fix Pictures dialog box
git-svn-id: svn://svn.code.sf.net/p/skychart/code/trunk@2743 79d5287f-89a4-4950-bfd8-701c3ebe4acb

skychart/pu_main.pas
@@ -3440,7 +3440,7 @@ end;
 
 procedure Tf_main.SetupPicturesExecute(Sender: TObject);
 begin
-ShowBackgroundImageExecute(sender);
+SetupPicturesPage(1);
 end;
 
 procedure Tf_main.SetupChartExecute(Sender: TObject);

commit 862c45cfc308414ac36af19ccd5c45fe40776faf

Fix "show picture" button, bug introduced by r2743.

skychart/pu_main.lfm
@@ -10891,8 +10891,10 @@ object f_main: Tf_main
       OnClick = SetupDisplayExecute
     end
     object MenuItem18: TMenuItem
-      Action = SetupPictures
-      OnClick = SetupPicturesExecute
+      Caption = 'Setup Pictures'
+      Hint = 'Change pictures display'
+      ImageIndex = 77
+      OnClick = MenuItem7Click
     end
     object MenuItem19: TMenuItem
       Action = SetupSystem

skychart/pu_main.pas
@@ -3442,7 +3442,7 @@ end;
 
 procedure Tf_main.SetupPicturesExecute(Sender: TObject);
 begin
-SetupPicturesPage(1);
+ShowBackgroundImageExecute(sender);
 end;
 
 procedure Tf_main.SetupChartExecute(Sender: TObject);
@@ -6779,6 +6779,7 @@ SetupInternet.caption:='&'+rsInternet+Ellipsis;
 SetupPictures.caption:='&'+rsPictures+Ellipsis;
 SetupCalendar.caption:='&'+rsCalendar+Ellipsis;
 MenuItem7.caption:='&'+rsPictures+Ellipsis;
+MenuItem18.caption:='&'+rsPictures+Ellipsis;
 SetupCatalog.caption:='&'+rsCatalog+Ellipsis;
 MenuItem8.caption:='&'+rsShowHideDSSI;
 View1.caption:='&'+rsView;