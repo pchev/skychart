2b98aa87de761f06eb215792ffc7a18eba3b4d59
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2014-08-07 09:24:10 +0000

    use standard strip command

--- trunk/install_win.sh
+++ trunk/install_win.sh
@@ -16,13 +16,13 @@ echo Install skychart $OS_TARGET to $destdir
 install -m 755 -d $destdir
 
 if [ $OS_TARGET = win32 ]; then 
-  i386-win32-strip -v -o $destdir/skychart.exe skychart/cdc.exe 
-  i386-win32-strip -v -o $destdir/cdcicon.exe skychart/cdcicon.exe
-  i386-win32-strip -v -o $destdir/varobs.exe varobs/varobs.exe
-  i386-win32-strip -v -o $destdir/varobs_lpv_bulletin.exe varobs/varobs_lpv_bulletin.exe
-  i386-win32-strip -v -o $destdir/libplan404.dll skychart/library/plan404/libplan404.dll
-  i386-win32-strip -v -o $destdir/libcdcwcs.dll skychart/library/wcs/libcdcwcs.dll
-  i386-win32-strip -v -o $destdir/libgetdss.dll skychart/library/getdss/libgetdss.dll
+  strip -v -o $destdir/skychart.exe skychart/cdc.exe 
+  strip -v -o $destdir/cdcicon.exe skychart/cdcicon.exe
+  strip -v -o $destdir/varobs.exe varobs/varobs.exe
+  strip -v -o $destdir/varobs_lpv_bulletin.exe varobs/varobs_lpv_bulletin.exe
+  strip -v -o $destdir/libplan404.dll skychart/library/plan404/libplan404.dll
+  strip -v -o $destdir/libcdcwcs.dll skychart/library/wcs/libcdcwcs.dll
+  strip -v -o $destdir/libgetdss.dll skychart/library/getdss/libgetdss.dll
   unzip -d $destdir system_integration/Windows/data/sqlite3.zip
   unzip -d $destdir system_integration/Windows/data/zlib.zip
   install -m 755 -d $destdir/data
@@ -32,13 +32,13 @@ if [ $OS_TARGET = win32 ]; then
   unzip -d $destdir/data/iridflar system_integration/Windows/data/dosbox.zip
 fi
 if [ $OS_TARGET = win64 ]; then
-  x86_64-win64-strip -v -o $destdir/skychart.exe skychart/cdc.exe
-  x86_64-win64-strip -v -o $destdir/cdcicon.exe skychart/cdcicon.exe
-  x86_64-win64-strip -v -o $destdir/varobs.exe varobs/varobs.exe
-  x86_64-win64-strip -v -o $destdir/varobs_lpv_bulletin.exe varobs/varobs_lpv_bulletin.exe
-  x86_64-win64-strip -v -o $destdir/libplan404.dll skychart/library/plan404/libplan404.dll
-  x86_64-win64-strip -v -o $destdir/libcdcwcs.dll skychart/library/wcs/libcdcwcs.dll
-  x86_64-win64-strip -v -o $destdir/libgetdss.dll skychart/library/getdss/libgetdss.dll
+  strip -v -o $destdir/skychart.exe skychart/cdc.exe
+  strip -v -o $destdir/cdcicon.exe skychart/cdcicon.exe
+  strip -v -o $destdir/varobs.exe varobs/varobs.exe
+  strip -v -o $destdir/varobs_lpv_bulletin.exe varobs/varobs_lpv_bulletin.exe
+  strip -v -o $destdir/libplan404.dll skychart/library/plan404/libplan404.dll
+  strip -v -o $destdir/libcdcwcs.dll skychart/library/wcs/libcdcwcs.dll
+  strip -v -o $destdir/libgetdss.dll skychart/library/getdss/libgetdss.dll
   unzip -d $destdir system_integration/Windows/data/sqlite3_x64.zip
   unzip -d $destdir system_integration/Windows/data/zlib_x64.zip
   install -m 755 -d $destdir/data
