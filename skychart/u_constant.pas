b32a32f2212e4075d7a8e531119e07c4feda09d2
Patrick Chevalley <pch@ap-i.net>
2014-09-03 11:22:45 +0000

    set unique menu accelerator key

--- skychart/u_constant.pas
+++ skychart/u_constant.pas
@@ -109,6 +109,7 @@ const
   DefaultPrtRes = 300;
   encryptpwd = 'zh6Tiq4h;90uA3.ert';
   ReservedScript=3;
+  MaxMenulevel=10;
   //                          0         1                                       5                                                 10                                                15                                                20                            23        24        25        26        27        28        29        30        31        32        33        34        35
   //                          sky       -0.3      -0.1      0.2       0.5       0.8       1.3       1.3+      galaxy    cluster   neb       -white-   az grid   eq grid   orbit     const     boundary  eyepiece  misc      horizon   asteroid  comet     milkyway  ColorAst  ColorOCl  ColorGCl  ColorPNe  ColorDN   ColorEN   ColorRN   ColorSN   ColorGxy  ColorGxyCl ColorQ   ColorGL   ColorNE
   DfColor: Starcolarray =
