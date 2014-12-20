d3759491bcc70a679f36d1e27e2196921355b810
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2014-12-20 11:53:49 +0000

    update for new Indi client

--- trunk/skychart/pu_indiclient.pas
+++ trunk/skychart/pu_indiclient.pas
@@ -53,8 +53,10 @@ type
     Edit1: TEdit;
     SpeedButton6: TButton;
     SpeedButton4: TButton;
+    InitTimer: TTimer;
     {Utility and form functions}
     procedure FormCreate(Sender: TObject);
+    procedure InitTimerTimer(Sender: TObject);
     procedure kill(Sender: TObject; var CanClose: Boolean);
     procedure ConnectClick(Sender: TObject);
     procedure SaveConfig;
@@ -67,12 +69,30 @@ type
     { Private declarations }
     client: TIndiBaseClient;
     TelescopeDevice: Basedevice;
-    CoordLock : boolean;
-    connected: boolean;
-    TelescopeJD: double;
-    Longitude : single;                 // Observatory longitude (Negative East of Greenwich}
-    Latitude : single;                  // Observatory latitude
-    curdeg_x,  curdeg_y :double;        // current equatorial position in degrees
+    scope_port: ITextVectorProperty;
+    coord_prop: INumberVectorProperty;
+    coord_ra:   INumber;
+    coord_dec:  INumber;
+    oncoordset_prop: ISwitchVectorProperty;
+    setslew_prop:    ISwitch;
+    settrack_prop:   ISwitch;
+    setsync_prop:    ISwitch;
+    abortmotion_prop:ISwitchVectorProperty;
+    abort_prop:      ISwitch;
+    GeographicCoord_prop:INumberVectorProperty;
+    SlewRate_prop:   ISwitchVectorProperty;
+    MotionRate_prop: INumberVectorProperty;
+    moveNS_prop:     ISwitchVectorProperty;
+    moveN_prop:      ISwitch;
+    moveS_prop:      ISwitch;
+    moveEW_prop:     ISwitchVectorProperty;
+    moveE_prop:      ISwitch;
+    moveW_prop:      ISwitch;
+    eod_coord:  boolean;
+    ready,connected: boolean;
+    SlewRateList: TStringList;
+    procedure ClearStatus;
+    procedure CheckStatus;
     procedure NewDevice(dp: Basedevice);
     procedure NewMessage(msg: string);
     procedure NewProperty(indiProp: IndiProperty);
@@ -95,9 +115,9 @@ type
     Procedure ScopeGetEqSys(var EqSys : double);
     Procedure ScopeSetObs(la,lo : double);
     Procedure ScopeAlign(source : string; ra,dec : single);
-    Procedure ScopeGetRaDec(var ar,de : double; var ok : boolean);
+    Procedure ScopeGetRaDec(var ra,de : double; var ok : boolean);
     Procedure ScopeGetAltAz(var alt,az : double; var ok : boolean);
-    Procedure ScopeGoto(ar,de : single; var ok : boolean);
+    Procedure ScopeGoto(ra,de : single; var ok : boolean);
     Procedure ScopeAbortSlew;
     Procedure ScopeReset;
     Function  ScopeInitialized : boolean ;
@@ -111,17 +131,51 @@ type
 implementation
 {$R *.lfm}
 
+procedure Tpop_indi.ClearStatus;
+begin
+    coord_prop:=nil;
+    abortmotion_prop:=nil;
+    SlewRate_prop:=nil;
+    MotionRate_prop:=nil;
+    moveNS_prop:=nil;
+    moveEW_prop:=nil;
+    oncoordset_prop:=nil;
+    GeographicCoord_prop:=nil;
+    TelescopeDevice:=nil;
+    scope_port:=nil;
+    ready:=false;
+    connected := false;
+    eod_coord:=true;
+    led.color:=clRed;
+    SlewRateList.Clear;
+end;
+
+procedure Tpop_indi.CheckStatus;
+begin
+    if connected and
+       (coord_prop<>nil) and
+       (oncoordset_prop<>nil)
+    then begin
+       ready:=true;
+       led.color:=clLime;
+    end;
+end;
+
+
 procedure Tpop_indi.ServerConnected(Sender: TObject);
 begin
    Memomsg.Lines.Add('Server connected');
-  // csc.IndiPort;
-  client.connectDevice(csc.IndiDevice);
+   if (scope_port<>nil)and(csc.IndiPort<>'') then begin
+      scope_port.tp[0].text:=csc.IndiPort;
+      client.sendNewText(scope_port);
+   end;
+   client.connectDevice(csc.IndiDevice);
 end;
 
 procedure Tpop_indi.ServerDisconnected(Sender: TObject);
 begin
   Memomsg.Lines.Add('Server disconnected');
-  connected:=false;
+  ClearStatus;
   led.color:=clRed;
 end;
 
@@ -130,7 +184,6 @@ begin
 //  Memomsg.Lines.Add('Newdev: '+dp.getDeviceName);
   if dp.getDeviceName=csc.IndiDevice then begin
      connected:=true;
-     led.color:=clLime;
      TelescopeDevice:=dp;
   end;
 end;
@@ -142,22 +195,78 @@ begin
 end;
 
 procedure Tpop_indi.NewProperty(indiProp: IndiProperty);
+var propname: string;
+    proptype: INDI_TYPE;
+    i: integer;
 begin
 //  Memomsg.Lines.Add('Newprop: '+indiProp.getDeviceName+' '+indiProp.getName+' '+indiProp.getLabel);
-  if indiProp.getType = INDI_NUMBER then NewNumber(indiProp.getNumber);
+  propname:=indiProp.getName;
+  proptype:=indiProp.getType;
+
+  if (proptype=INDI_TEXT)and(propname='DEVICE_PORT') then begin
+     scope_port:=indiProp.getText;
+  end
+  else if (proptype=INDI_NUMBER)and(propname='EQUATORIAL_EOD_COORD') then begin
+     coord_prop:=indiProp.getNumber;
+     coord_ra:=IUFindNumber(coord_prop,'RA');
+     coord_dec:=IUFindNumber(coord_prop,'DEC');
+     eod_coord:=true;
+     NewNumber(coord_prop);
+  end
+  else if (proptype=INDI_NUMBER)and(coord_prop=nil)and(propname='EQUATORIAL_COORD') then begin
+     coord_prop:=indiProp.getNumber;
+     coord_ra:=IUFindNumber(coord_prop,'RA');
+     coord_dec:=IUFindNumber(coord_prop,'DEC');
+     eod_coord:=false;
+     NewNumber(coord_prop);
+  end
+  else if (proptype=INDI_SWITCH)and(propname='ON_COORD_SET') then begin
+     oncoordset_prop:=indiProp.getSwitch;
+     setslew_prop:=IUFindSwitch(oncoordset_prop,'SLEW');
+     settrack_prop:=IUFindSwitch(oncoordset_prop,'TRACK');
+     setsync_prop:=IUFindSwitch(oncoordset_prop,'SYNC');
+  end
+  else if (proptype=INDI_SWITCH)and(propname='TELESCOPE_ABORT_MOTION') then begin
+     abortmotion_prop:=indiProp.getSwitch;
+     abort_prop:=IUFindSwitch(abortmotion_prop,'ABORT');
+     if abort_prop=nil then abort_prop:=IUFindSwitch(abortmotion_prop,'ABORT_MOTION');
+  end
+  else if (proptype=INDI_NUMBER)and(propname='GEOGRAPHIC_COORD') then begin
+
+  end
+  else if (proptype=INDI_SWITCH)and(propname='Slew Rate') then begin
+     SlewRateList.Clear;
+     SlewRate_prop:=indiProp.getSwitch;
+     for i:=0 to SlewRate_prop.nsp-1 do begin
+        SlewRateList.Add(SlewRate_prop.sp[i].name);
+     end;
+  end
+  else if (proptype=INDI_NUMBER)and(propname='TELESCOPE_MOTION_RATE') then begin
+     MotionRate_prop:=indiProp.getNumber;
+  end
+  else if (proptype=INDI_SWITCH)and(propname='TELESCOPE_MOTION_NS') then begin
+     moveNS_prop:=indiProp.getSwitch;
+     moveN_prop:=IUFindSwitch(moveNS_prop,'MOTION_NORTH');
+     moveS_prop:=IUFindSwitch(moveNS_prop,'MOTION_SOUTH');
+  end
+  else if (proptype=INDI_SWITCH)and(propname='TELESCOPE_MOTION_WE') then begin
+     moveEW_prop:=indiProp.getSwitch;
+     moveE_prop:=IUFindSwitch(moveEW_prop,'MOTION_EAST');
+     moveW_prop:=IUFindSwitch(moveEW_prop,'MOTION_WEST');
+  end
+  else if (proptype=INDI_SWITCH)and(propname='CONNECTION') then begin
+
+  end;
+  CheckStatus;
 end;
 
 procedure Tpop_indi.NewNumber(nvp: INumberVectorProperty);
 var n: INumber;
 begin
 //  Memomsg.Lines.Add('NewNumber: '+nvp.name+' '+FloatToStr(nvp.np[0].value));
-  if nvp.name='EQUATORIAL_EOD_COORD' then begin
-     n:=IUFindNumber(nvp,'RA');
-     Curdeg_x:=n.value*15;
-     n:=IUFindNumber(nvp,'DEC');
-     Curdeg_y:=n.value;
-     pos_x.text := artostr(Curdeg_x/15);
-     pos_y.text := detostr(Curdeg_y);
+  if nvp=coord_prop then begin
+     pos_x.text := artostr(coord_ra.value);
+     pos_y.text := detostr(coord_dec.value);
   end;
 end;
 
@@ -181,7 +290,6 @@ var i: integer;
 begin
 pos_x.text:='';
 pos_y.text:='';
-if trim(edit1.text)='' then exit;
 if (client<>nil) then begin
    client.Terminate;
 end;
@@ -191,6 +299,7 @@ if IndiProcess.Active then begin
    IndiProcess.Executable:='';
    IndiProcess.Parameters.Clear;
 end;
+ClearStatus;
 ok:=true;
 end;
 
@@ -203,7 +312,6 @@ if not connected then begin
   led.color:=clRed;
   Memomsg.Clear;
   led.refresh;
-  TelescopeJD:=0;
   if csc.IndiAutostart and (not IndiProcess.Active) then begin
      buf:=GetCurrentDir;
      try
@@ -250,6 +358,8 @@ if not connected then begin
   client.SetServer(csc.IndiServerHost,csc.IndiServerPort);
   client.watchDevice(csc.IndiDevice);
   client.ConnectServer;
+  led.color:=clYellow;
+  InitTimer.Enabled:=true;
   ok:=true;
 end
 else Memomsg.Lines.Add('Already connected');
@@ -277,11 +387,11 @@ showmodal;
 ok:=(modalresult=mrOK);
 end;
 
-Procedure Tpop_indi.ScopeGetRaDec(var ar,de : double; var ok : boolean);
+Procedure Tpop_indi.ScopeGetRaDec(var ra,de : double; var ok : boolean);
 begin
-   if ScopeConnected then begin
-         ar:=Curdeg_x/15;
-         de:=Curdeg_y;
+   if ScopeConnected and (coord_prop<>nil) then begin
+         ra:=coord_ra.value;
+         de:=coord_dec.value;
          ok:=true;
    end else ok:=false;
 end;
@@ -293,11 +403,11 @@ end;
 
 Procedure Tpop_indi.ScopeGetInfo(var scName : shortstring; var QueryOK,SyncOK,GotoOK : boolean; var refreshrate : integer);
 begin
-   if ScopeConnected and (client.devices.Count>0) then begin
-     scname:=BaseDevice(client.devices[0]).getDeviceName;
-     QueryOK:=true;
-     SyncOK:=BaseDevice(client.devices[0]).getSwitch('ON_COORD_SET')<>nil;
-     GotoOK:=SyncOK;
+   if ScopeConnected and ready then begin
+     scname:=TelescopeDevice.getDeviceName;
+     QueryOK:=(coord_prop<>nil);
+     SyncOK:=(coord_prop<>nil)and(oncoordset_prop<>nil)and(setsync_prop<>nil);
+     GotoOK:=(coord_prop<>nil)and(oncoordset_prop<>nil)and(setslew_prop<>nil);
    end else begin
       scname:='';
       QueryOK:=false;
@@ -309,9 +419,9 @@ end;
 
 Procedure Tpop_indi.ScopeGetEqSys(var EqSys : double);
 begin
-if ScopeConnected  and (client.devices.Count>0) then begin
-   if BaseDevice(client.devices[0]).getNumber('EQUATORIAL_EOD_COORD')<>nil then EqSys:=0
-      else EqSys:=2000;
+if ScopeConnected  and ready then begin
+   if eod_coord then EqSys:=0
+                else EqSys:=2000;
 end;
 end;
 
@@ -321,46 +431,36 @@ end;
 
 Procedure Tpop_indi.ScopeSetObs(la,lo : double);
 begin
-latitude:=la;
-longitude:=-lo;
+//latitude:=la;
+//longitude:=-lo;
 end;
 
 Procedure Tpop_indi.ScopeAlign(source : string; ra,dec : single);
-var svp:ISwitchVectorProperty;
-    s:ISwitch;
-    nvp:INumberVectorProperty;
-    np:INumber;
 begin
- svp:=TelescopeDevice.getSwitch('ON_COORD_SET');
- IUResetSwitch(svp);
- s:=IUFindSwitch(svp,'SYNC');
- s.s:=ISS_ON;
- client.sendNewSwitch(svp);
- nvp:=TelescopeDevice.getNumber('EQUATORIAL_EOD_COORD');
- np:=IUFindNumber(nvp,'RA');
- np.value:=ra;
- np:=IUFindNumber(nvp,'DEC');
- np.value:=dec;
- client.sendNewNumber(nvp);
+ if ready then begin
+   IUResetSwitch(oncoordset_prop);
+   setsync_prop.s:=ISS_ON;
+   client.sendNewSwitch(oncoordset_prop);
+   coord_ra.value:=ra;
+   coord_dec.value:=dec;
+   client.sendNewNumber(coord_prop);
+ end;
 end;
 
-Procedure Tpop_indi.ScopeGoto(ar,de : single; var ok : boolean);
+Procedure Tpop_indi.ScopeGoto(ra,de : single; var ok : boolean);
 var svp:ISwitchVectorProperty;
     s:ISwitch;
     nvp:INumberVectorProperty;
     np:INumber;
 begin
- svp:=TelescopeDevice.getSwitch('ON_COORD_SET');
- IUResetSwitch(svp);
- s:=IUFindSwitch(svp,'SLEW');
- s.s:=ISS_ON;
- client.sendNewSwitch(svp);
- nvp:=TelescopeDevice.getNumber('EQUATORIAL_EOD_COORD');
- np:=IUFindNumber(nvp,'RA');
- np.value:=ar;
- np:=IUFindNumber(nvp,'DEC');
- np.value:=de;
- client.sendNewNumber(nvp);
+ if ready then begin
+   IUResetSwitch(oncoordset_prop);
+   setslew_prop.s:=ISS_ON;
+   client.sendNewSwitch(oncoordset_prop);
+   coord_ra.value:=ra;
+   coord_dec.value:=de;
+   client.sendNewNumber(coord_prop);
+ end;
 end;
 
 Procedure Tpop_indi.ScopeReadConfig(ConfigPath : shortstring);
@@ -369,57 +469,63 @@ begin
 end;
 
 Procedure Tpop_indi.ScopeAbortSlew;
-var svp:ISwitchVectorProperty;
-    s:ISwitch;
 begin
-if TelescopeDevice=nil then exit;
-svp:=TelescopeDevice.getSwitch('TELESCOPE_ABORT_MOTION');
-if svp=nil then exit;
-IUResetSwitch(svp);
-s:=IUFindSwitch(svp,'ABORT');
-if s=nil then s:=IUFindSwitch(svp,'ABORT_MOTION');
-if s=nil then exit;
-s.s:=ISS_ON;
-client.sendNewSwitch(svp);
+if (abortmotion_prop<>nil) then begin
+   IUResetSwitch(abortmotion_prop);
+   abort_prop.s:=ISS_ON;
+   client.sendNewSwitch(abortmotion_prop);
+end;
 end;
 
 procedure Tpop_indi.GetScopeRates(var nrates:integer;var srate: TStringList);
 begin
-{  indi1.GetSlewRate(srate);
-  if srate.Count=0 then srate.Add('N/A');
-  nrates:=srate.count;}
+srate.Clear;
+if MotionRate_prop<>nil then begin
+   // no driver at the moment
+end
+else if SlewRate_prop<>nil then begin
+  srate.Assign(SlewRateList);
+end;
+if srate.Count=0 then srate.Add('N/A');
+nrates:=srate.count;
 end;
 
 procedure Tpop_indi.ScopeMoveAxis(axis:Integer; rate: string);
 var dir1,dir2: string;
     positive: boolean;
+    sw:ISwitch;
 begin
-{positive:=(copy(rate,1,1)<>'-');
-if not positive then delete(rate,1,1);
-case axis of
-  0: begin  //  alpha
-       if positive then begin
-          dir1:='Off';
-          dir2:='On';
-       end else begin
-          dir1:='On';
-          dir2:='Off';
+if (moveNS_prop<>nil) and (moveEW_prop<>nil) then begin
+  positive:=(copy(rate,1,1)<>'-');
+  if not positive then delete(rate,1,1);
+  if MotionRate_prop<>nil then begin
+     // no driver at the moment
+  end
+  else if SlewRate_prop<>nil then begin
+    if pos('N/A',rate)=0 then begin
+       sw:=IUFindSwitch(SlewRate_prop,rate);
+       if sw<>nil then begin
+         IUResetSwitch(SlewRate_prop);
+         sw.s:=ISS_ON;
+         client.sendNewSwitch(SlewRate_prop);
        end;
-       if pos('N/A',rate)=0 then indi1.SetSlewRate(rate);
-       indi1.MotionWE(dir1,dir2);
-     end;
-  1: begin  // delta
-       if positive then begin
-          dir1:='On';
-          dir2:='Off';
-       end else begin
-          dir1:='Off';
-          dir2:='On';
+    end;
+  end;
+  case axis of
+    0: begin  //  alpha
+         IUResetSwitch(moveEW_prop);
+         if positive then moveW_prop.s:=ISS_ON
+                     else moveE_prop.s:=ISS_ON;
+         client.sendNewSwitch(moveEW_prop);
        end;
-       if pos('N/A',rate)=0 then indi1.SetSlewRate(rate);
-       indi1.MotionNS(dir1,dir2);
-     end;
-end;   }
+    1: begin  // delta
+         IUResetSwitch(moveNS_prop);
+         if positive then moveN_prop.s:=ISS_ON
+                     else moveS_prop.s:=ISS_ON;
+         client.sendNewSwitch(moveNS_prop);
+       end;
+  end;
+end;
 end;
 
 {-------------------------------------------------------------------------------
@@ -464,8 +570,19 @@ end;
 
 procedure Tpop_indi.FormCreate(Sender: TObject);
 begin
-CoordLock := false;
-connected := false;
+SlewRateList:=TStringList.Create;
+ClearStatus;
+end;
+
+procedure Tpop_indi.InitTimerTimer(Sender: TObject);
+var ok:boolean;
+begin
+  InitTimer.Enabled:=false;
+  if TelescopeDevice=nil then begin
+     Memomsg.Lines.Add('No response from server');
+     Memomsg.Lines.Add('Is driver"'+csc.IndiDevice+'" running?');
+     ScopeDisconnect(ok);
+  end;
 end;
 
 procedure Tpop_indi.ConnectClick(Sender: TObject);
@@ -503,6 +620,7 @@ end;
 procedure Tpop_indi.FormDestroy(Sender: TObject);
 begin
 SaveConfig;
+SlewRateList.Free;
 end;
 
 end.
