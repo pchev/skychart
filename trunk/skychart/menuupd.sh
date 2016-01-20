7493cdbf8132334c2a8bc873dd34c14d4118df8a
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2016-01-20 15:54:10 +0000

    Update script translations

--- trunk/skychart/menuupd.sh
+++ trunk/skychart/menuupd.sh
@@ -1,1 +1,3 @@
+# Update the menu item list for scripting reference
+
 grep TMenuItem pu_main.lfm | sed 's/object//'| sed 's/: TMenuItem//' | grep -v N[0123456789]| grep -v MenuItem[0123456789]| grep -v topmessage| grep -v ResetAllLabels1 > menu.txt
