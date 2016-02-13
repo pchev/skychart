8323b34da916e013d6576a86a48ad73ad972c6ee
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2016-02-13 14:54:31 +0000

    horizon depression only if the altitude is positive

--- trunk/skychart/cu_skychart.pas
+++ trunk/skychart/cu_skychart.pas
@@ -632,13 +632,18 @@ begin
    cfgsc.ObsRefractionCor:=(cfgsc.ObsPressure/1010)*(283/(273+cfgsc.ObsTemperature));
    sla_REFCO(cfgsc.ObsAltitude,273.15+cfgsc.ObsTemperature,cfgsc.ObsPressure,cfgsc.ObsRH,RefractionWavelength,deg2rad*cfgsc.ObsLatitude,cfgsc.ObsTlr,1E-8,cfgsc.ObsRefA,cfgsc.ObsRefb);
    // horizon dip  (Wittmann, 1997 Astron. Nachr. 318)
-   r90:=0;
-   Refraction(r90,true,cfgsc,2);
-   r90:=rad2deg*r90;
-   rm:= r90*((2*exp(cfgsc.ObsAltitude/8000))/(1+exp(-cfgsc.ObsAltitude/12300)));
-   dip:=sqrt(cfgsc.ObsAltitude)*0.02931;
-   ht:=dip+rm;
-   cfgsc.ObsHorizonDepression:=min(0,-deg2rad*ht);
+   if cfgsc.ShowHorizonDepression and (cfgsc.ObsAltitude>10) then begin
+     r90:=0;
+     Refraction(r90,true,cfgsc,2);
+     r90:=rad2deg*r90;
+     rm:= r90*((2*exp(cfgsc.ObsAltitude/8000))/(1+exp(-cfgsc.ObsAltitude/12300)));
+     dip:=sqrt(cfgsc.ObsAltitude)*0.02931;
+     ht:=dip+rm;
+     cfgsc.ObsHorizonDepression:=min(0,-deg2rad*ht);
+   end else begin
+     cfgsc.ShowHorizonDepression:=false;
+     cfgsc.ObsHorizonDepression:=0;
+   end;
    // diurnal abberation
    sla_GEOC(p,cfgsc.ObsAltitude,cfgsc.ObsRAU,cfgsc.ObsZAU);
    cfgsc.Diurab := PI2*cfgsc.ObsRAU*SOLSID/C;
