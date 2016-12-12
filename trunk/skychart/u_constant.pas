7622e9544a70998db8364d8bc5cdea2ca6202484
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2016-12-12 22:05:35 +0000

    add missing file

--- trunk/skychart/u_constant.pas
+++ trunk/skychart/u_constant.pas
@@ -47,7 +47,7 @@ type
 
 const
   cdcversion = '3.11-svn';
-  cdcver = '3.11n';
+  cdcver = '3.11o';
   cdccpy = 'Copyright (C) 2002-2016 Patrick Chevalley';
   cdcauthors = 'Patrick Chevalley, pch@ap-i.net' + crlf +
     'Peter Dean,' + crlf +
