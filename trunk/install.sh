b1d47ef1e78a4e4ac120bdcf669aee8967f3199a
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2017-10-03 09:01:35 +0000

    move appdata.xml

--- trunk/install.sh
+++ trunk/install.sh
@@ -13,7 +13,7 @@ echo Install skychart to $destdir
 install -m 755 -d $destdir
 install -m 755 -d $destdir/bin
 install -m 755 -d $destdir/share
-install -m 755 -d $destdir/share/appdata
+install -m 755 -d $destdir/share/metainfo
 install -m 755 -d $destdir/share/applications
 install -m 755 -d $destdir/share/doc
 install -m 755 -d $destdir/share/doc/skychart
@@ -30,7 +30,7 @@ install -v -m 755 -s skychart/cdcicon  $destdir/bin/cdcicon
 install -v -m 755 -s varobs/varobs  $destdir/bin/varobs
 install -v -m 755 -s varobs/varobs_lpv_bulletin  $destdir/bin/varobs_lpv_bulletin
 install -v -m 644 system_integration/Linux/share/applications/skychart.desktop $destdir/share/applications/skychart.desktop
-install -v -m 644 system_integration/Linux/share/appdata/skychart.appdata.xml $destdir/share/appdata/skychart.appdata.xml
+install -v -m 644 system_integration/Linux/share/metainfo/skychart.appdata.xml $destdir/share/metainfo/skychart.appdata.xml
 install -v -m 644 system_integration/Linux/share/doc/skychart/changelog $destdir/share/doc/skychart/changelog
 install -v -m 644 system_integration/Linux/share/doc/skychart/copyright $destdir/share/doc/skychart/copyright
 install -v -m 644 system_integration/Linux/share/pixmaps/skychart.png $destdir/share/pixmaps/skychart.png
