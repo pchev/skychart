5f4e684aed16a7bc4bddb74da4f957559298e807
Patrick Chevalley <pch@ap-i.net>
2017-11-23 12:25:07 +0000

    Clarify what simulation movement is.

--- tools/data/language/searchlang.sh
+++ tools/data/language/searchlang.sh
@@ -1,10 +1,10 @@
-langs='ca cs de el en es fa fr hr hu is it ja ml nb nl no pl pt_BR pt ru si sl sv th tr uk zh_CN zh zh_TW'
+langs='ca cs de el en es fa fr hr hu is it ja ml mr nb nl no oc pl pt_BR pt ru si sk sl sv th tr uk zh_CN zh zh_TW'
 
 for lg in $langs; do
 echo '======================================================================'
 echo $lg
 #### repeat this block for each text
-txt="Display the chart information in the menu bar"
+txt="Plot the position of the moving object."
 echo $txt
 sed -n "/$txt/ {p;n;p} " skychart.$lg.po
 echo
