e928beeecdcfbd8065c0dab78774ca650e9a4fcb
pchev <pchev@79d5287f-89a4-4950-bfd8-701c3ebe4acb>
2017-12-07 09:21:29 +0000

    Fix Mac build

--- trunk/buildpkg_mac.sh
+++ trunk/buildpkg_mac.sh
@@ -30,7 +30,7 @@ if [[ $lastrev -ne $currentrev ]]; then
   rm -rf $basedir
 
 # make i386 Mac version
-  ./configure $configopt prefix=$builddir target=i386-darwin
+  ./configure $configopt prefix=$builddir target=i386-darwin,i386
   if [[ $? -ne 0 ]]; then exit 1;fi
   make CPU_TARGET=i386 clean
   fpcopts="-CX -XX -Xs -Cs800000" make CPU_TARGET=i386
