beb886ba8ff60f815e68db971e3156fb0417bc5c
Patrick Chevalley <pch@ap-i.net>
2018-07-04 16:43:44 +0200

    Fix unused translation also check for script

--- skychart/unused_translation.sh
+++ skychart/unused_translation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-files=$(ls *.pas | grep -v translation)
+files=$(find *.pas ../tools/data/script/*.cdcps | grep -v translation)
 trans=$(grep "  rs" u_translation.pas | cut -d = -f1)
 for t in $trans ; do
-  grep  -w $t $files > /dev/null 2>&1
+  grep -i -w $t $files > /dev/null 2>&1
   if [[ $? != 0 ]]; then
      echo $t
   fi
