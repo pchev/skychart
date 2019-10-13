949e536e47d1b0486719d8db5c87562d1264eed3
Patrick Chevalley <pch@ap-i.net>
2019-10-13 12:31:11 +0200

    Update icons from https://github.com/mattiaverga/skychart-breeze-theme

--- tools/data/Themes/src/generateIcons.py
+++ tools/data/Themes/src/generateIcons.py
@@ -1,35 +1,71 @@
-# A script to generate png icons from svgs
-# Probably not the best, but it does the work
-# It needs Inkscape installed on the system
+# -*- coding:utf-8 -*-
+#
+# MIT License
+#
+# Copyright (c) 2019 Mattia Verga <mattia.verga@tiscali.it>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+"""A simple script to generate png icons from svgs.
+Probably not the best, but it does the work.
+It needs Inkscape installed on the system.
+"""
 
 import os
+import sys
 
-currDir = os.path.dirname(os.path.realpath(__file__))
-svgDir = currDir + os.sep + "svg"
-pngDir = currDir + os.sep + "icons"
+svgDir = os.path.join(os.path.dirname(__file__), 'svg')
+pngDir = os.path.join(os.path.dirname(__file__), 'icons')
+styles = ('daylight', 'night-vision')
+# Use 16px SVGs only for 16px icons and 22px SVGs for upscale
+mapSizes = {'16': ('16', ),
+            '22': ('22', '32', '48', '64', )}
 
-with open('corrispondenze.txt', 'r') as indexFile :
-	for line in indexFile :
-		record = line.split('\t')
-		
-		for style in ("daylight","night-vision") :
-			for svgSize in ("16","22") :
-				svgFile = svgDir + os.sep + style + os.sep + svgSize+"x"+svgSize + os.sep + record[1].strip() + ".svg"
-				#Check files existence
-				if not os.path.isfile(svgFile) :
-					print(svgFile + " not found")
-					sys.exit()
-				#Use 16px SVGs only for 16px icons and 22px SVGs for upscale
-				if svgSize == "22" :
-					for pngSize in ("22","32","64") :
-						#Check png directory existence
-						if not os.path.exists(pngDir + os.sep + style + os.sep + pngSize+"x"+pngSize) :
-							os.makedirs(pngDir + os.sep + style + os.sep + pngSize+"x"+pngSize)
-						pngFile = pngDir + os.sep + style + os.sep + pngSize+"x"+pngSize + os.sep + record[0].strip() + ".png"
-						os.system("inkscape -z -f \"" + svgFile +"\" -w " + pngSize + " -e \"" + pngFile + "\"")
-				else :
-					#Check png directory existence
-					if not os.path.exists(pngDir + os.sep + style + os.sep + svgSize+"x"+svgSize) :
-						os.makedirs(pngDir + os.sep + style + os.sep + svgSize+"x"+svgSize)
-					pngFile = pngDir + os.sep + style + os.sep + svgSize+"x"+svgSize + os.sep + record[0].strip() + ".png"
-					os.system("inkscape -z -f \"" + svgFile +"\" -w " + svgSize + " -e \"" + pngFile + "\"")
+with open('corrispondenze.txt', 'r') as indexFile:
+    for line in indexFile:
+        record = line.split('\t')
+
+        for style in styles:
+            for svgSize in mapSizes.keys():
+                svgFile = os.path.join(svgDir,
+                                       style,
+                                       f'{svgSize}x{svgSize}',
+                                       f'{record[1].strip()}.svg'
+                                       )
+                # Check files existence
+                if not os.path.isfile(svgFile):
+                    print(f'{svgFile} not found')
+                    sys.exit()
+
+                for pngSize in mapSizes[svgSize]:
+                    # Check png directory existence
+                    if not os.path.exists(os.path.join(pngDir,
+                                                       style,
+                                                       f'{pngSize}x{pngSize}'
+                                                       )):
+                        os.makedirs(os.path.join(pngDir, style, f'{pngSize}x{pngSize}'))
+
+                    pngFile = os.path.join(pngDir,
+                                           style,
+                                           f'{pngSize}x{pngSize}',
+                                           f'{record[0].strip()}.png'
+                                           )
+                    # Create icon only if not existent
+                    if not os.path.isfile(pngFile):
+                        os.system(f'inkscape -z -f "{svgFile}" -w {pngSize} -e "{pngFile}"')
