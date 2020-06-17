db37bf556d24c0ea4b393f03a481dace45ea4d26
Patrick Chevalley <pch@ap-i.net>
2020-06-17 14:36:35 +0200

    Add missing netcat example script

---
+++ skychart/sample_client/script/client2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# Example of shell script to automate skychart using netcat 
+# Produce 640x480 chart of M1 in /tmp directory
+#
+
+# get CdC host and port
+cdchost=localhost
+cdcport=$(cat $HOME/.skychart/tmp/tcpport)
+
+# send a single command
+echo resize 640 480 | nc -C -w1 $cdchost $cdcport
+
+# send a group of command
+nc -C -w1 $cdchost $cdcport << QUIT
+setfov 15
+setproj EQUAT
+search M1
+redraw
+saveimg PNG /tmp/m1.png 0
+QUIT 
