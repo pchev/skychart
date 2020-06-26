561068479cef4c270a3513f5711daba0e12fcc8d
Patrick Chevalley <pch@ap-i.net>
2020-06-26 20:21:07 +0200

    Do not change script itself

--- skychart/library/wcs/1_cdcfix.sh
+++ skychart/library/wcs/1_cdcfix.sh
@@ -6,7 +6,7 @@
 sed -i 's/51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA/51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA/' *
 
 # fix name conflict with mingw
-sed -i 's/wcsrev1(/wcsrev1(/g' *
-sed -i 's/wcsset1(/wcsset1(/g' *
-sed -i 's/wcsrev1 (/wcsrev1 (/g' *
-sed -i 's/wcsset1 (/wcsset1 (/g' *
+sed -i 's/wcsrev(/wcsrev1(/g' *.c *.h
+sed -i 's/wcsset(/wcsset1(/g' *.c *.h
+sed -i 's/wcsrev (/wcsrev1 (/g' *.c *.h
+sed -i 's/wcsset (/wcsset1 (/g' *.c *.h
