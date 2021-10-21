056722b9d1607f1acac6156b80a924f8eb432d15
Patrick Chevalley <pch@ap-i.net>
2021-10-21 11:02:36 +0200

    Better definition for Skychart icon, by Mattia Verga

--- install.sh
+++ install.sh
@@ -20,8 +20,12 @@ install -m 755 -d $destdir/share/doc/skychart
 install -m 755 -d $destdir/share/pixmaps
 install -m 755 -d $destdir/share/icons
 install -m 755 -d $destdir/share/icons/hicolor
+install -m 755 -d $destdir/share/icons/hicolor/32x32
+install -m 755 -d $destdir/share/icons/hicolor/32x32/apps
 install -m 755 -d $destdir/share/icons/hicolor/48x48
 install -m 755 -d $destdir/share/icons/hicolor/48x48/apps
+install -m 755 -d $destdir/share/icons/hicolor/96x96
+install -m 755 -d $destdir/share/icons/hicolor/96x96/apps
 install -m 755 -d $destdir/share/icons/hicolor/scalable
 install -m 755 -d $destdir/share/icons/hicolor/scalable/apps
 
@@ -34,6 +38,8 @@ install -v -m 644 system_integration/Linux/share/metainfo/skychart.appdata.xml $
 install -v -m 644 system_integration/Linux/share/doc/skychart/changelog $destdir/share/doc/skychart/changelog
 install -v -m 644 system_integration/Linux/share/doc/skychart/copyright $destdir/share/doc/skychart/copyright
 install -v -m 644 system_integration/Linux/share/pixmaps/skychart.png $destdir/share/pixmaps/skychart.png
+install -v -m 644 system_integration/Linux/share/icons/hicolor/32x32/apps/skychart.png $destdir/share/icons/hicolor/32x32/apps/skychart.png
 install -v -m 644 system_integration/Linux/share/icons/hicolor/48x48/apps/skychart.png $destdir/share/icons/hicolor/48x48/apps/skychart.png
+install -v -m 644 system_integration/Linux/share/icons/hicolor/96x96/apps/skychart.png $destdir/share/icons/hicolor/96x96/apps/skychart.png
 install -v -m 644 system_integration/Linux/share/icons/hicolor/scalable/apps/skychart.svg $destdir/share/icons/hicolor/scalable/apps/skychart.svg
 
--- skychart/bitmaps/mainicon/cdc_icon.bat
+++
@@ -1,3 +0,0 @@
-CLS
-ECHO if not found, windres.exe is in lazarus/pp/bin
-windres cdc_icon.rc cdc_icon.res
