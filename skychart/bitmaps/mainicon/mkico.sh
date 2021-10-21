445cbb1b81f49257590ce69309de10a6be410d33
Patrick Chevalley <pch@ap-i.net>
2021-10-21 19:21:57 +0200

    Fix icon transparency

---
+++ skychart/bitmaps/mainicon/mkico.sh
@@ -0,0 +1,5 @@
+inkscape -w 128 -h 128 -o skychart128.png skychart.svg
+inkscape -w 96 -h 96 -o skychart96.png skychart.svg
+inkscape -w 48 -h 48 -o skychart48.png skychart.svg
+inkscape -w 32 -h 32 -o skychart32.png skychart.svg
+png2icns cdcIcon2.icns skychart32.png skychart48.png skychart128.png
