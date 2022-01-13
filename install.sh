c533cec96f522b9dbf9322d859e189ecd959fd38
Mattia Verga <mattia.verga@tiscali.it>
2022-01-13 16:21:33 +0100

    Add varobs new icon, desktop and appdata files

--- install.sh
+++ install.sh
@@ -37,19 +37,25 @@ install -v -m 755 -s skychart/catgen  $destdir/bin/catgen
 install -v -m 755 -s varobs/varobs  $destdir/bin/varobs
 install -v -m 644 system_integration/Linux/share/applications/net.ap_i.skychart.desktop $destdir/share/applications/net.ap_i.skychart.desktop
 install -v -m 644 system_integration/Linux/share/applications/net.ap_i.catgen.desktop $destdir/share/applications/net.ap_i.catgen.desktop
+install -v -m 644 system_integration/Linux/share/applications/net.ap_i.varobs.desktop $destdir/share/applications/net.ap_i.varobs.desktop
 install -v -m 644 system_integration/Linux/share/metainfo/net.ap_i.skychart.metainfo.xml $destdir/share/metainfo/net.ap_i.skychart.metainfo.xml
 install -v -m 644 system_integration/Linux/share/metainfo/net.ap_i.catgen.metainfo.xml $destdir/share/metainfo/net.ap_i.catgen.metainfo.xml
+install -v -m 644 system_integration/Linux/share/metainfo/net.ap_i.varobs.metainfo.xml $destdir/share/metainfo/net.ap_i.varobs.metainfo.xml
 install -v -m 644 system_integration/Linux/share/mime/packages/net.ap_i.skychart.xml $destdir/share/mime/packages/net.ap_i.skychart.xml
 install -v -m 644 system_integration/Linux/share/doc/skychart/changelog $destdir/share/doc/skychart/changelog
 install -v -m 644 system_integration/Linux/share/doc/skychart/copyright $destdir/share/doc/skychart/copyright
 install -v -m 644 system_integration/Linux/share/pixmaps/skychart.png $destdir/share/pixmaps/skychart.png
 install -v -m 644 system_integration/Linux/share/pixmaps/catgen.png $destdir/share/pixmaps/catgen.png
+install -v -m 644 system_integration/Linux/share/pixmaps/varobs.png $destdir/share/pixmaps/varobs.png
 install -v -m 644 system_integration/Linux/share/icons/hicolor/32x32/apps/skychart.png $destdir/share/icons/hicolor/32x32/apps/skychart.png
 install -v -m 644 system_integration/Linux/share/icons/hicolor/32x32/apps/catgen.png $destdir/share/icons/hicolor/32x32/apps/catgen.png
+install -v -m 644 system_integration/Linux/share/icons/hicolor/32x32/apps/varobs.png $destdir/share/icons/hicolor/32x32/apps/varobs.png
 install -v -m 644 system_integration/Linux/share/icons/hicolor/48x48/apps/skychart.png $destdir/share/icons/hicolor/48x48/apps/skychart.png
 install -v -m 644 system_integration/Linux/share/icons/hicolor/48x48/apps/catgen.png $destdir/share/icons/hicolor/48x48/apps/catgen.png
+install -v -m 644 system_integration/Linux/share/icons/hicolor/48x48/apps/varobs.png $destdir/share/icons/hicolor/48x48/apps/varobs.png
 install -v -m 644 system_integration/Linux/share/icons/hicolor/96x96/apps/skychart.png $destdir/share/icons/hicolor/96x96/apps/skychart.png
 install -v -m 644 system_integration/Linux/share/icons/hicolor/96x96/apps/catgen.png $destdir/share/icons/hicolor/96x96/apps/catgen.png
+install -v -m 644 system_integration/Linux/share/icons/hicolor/96x96/apps/varobs.png $destdir/share/icons/hicolor/96x96/apps/varobs.png
 install -v -m 644 system_integration/Linux/share/icons/hicolor/scalable/apps/skychart.svg $destdir/share/icons/hicolor/scalable/apps/skychart.svg
 install -v -m 644 system_integration/Linux/share/icons/hicolor/scalable/apps/catgen.svg $destdir/share/icons/hicolor/scalable/apps/catgen.svg
-
+install -v -m 644 system_integration/Linux/share/icons/hicolor/scalable/apps/varobs.svg $destdir/share/icons/hicolor/scalable/apps/varobs.svg
---
+++ varobs/bitmaps/mainicon/mkico.sh
@@ -0,0 +1,5 @@
+inkscape -w 128 -h 128 -o varobs128.png varobs.svg
+inkscape -w 96 -h 96 -o varobs96.png varobs.svg
+inkscape -w 48 -h 48 -o varobs48.png varobs.svg
+inkscape -w 32 -h 32 -o varobs32.png varobs.svg
+png2icns varobs.icns varobs32.png varobs48.png varobs128.png
