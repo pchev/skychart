26085a03aecd9b13c2e9233b0fdb332d2cac36df
Patrick Chevalley <pch@ap-i.net>
2024-03-03 14:27:53 +0100

    Change filter for VSX

---
+++ tools/cat/vsx/filtervsx.pp
@@ -0,0 +1,58 @@
+program filtervsx;
+{
+  This program filter the VSX data file available from CDS
+  It produce one data file with all the stars flagged as variable,
+  and a second file with only the bright visual variables.
+}
+
+uses sysutils;
+
+var fi,fo1,fo2: textfile;
+    row,buf,varflag,mflag: string;
+    m1,m2: double;
+    band1,band2: Char;
+
+begin
+  Assign(fi,'vsx.dat');
+  Reset(fi);
+  Assign(fo1,'vsxv.dat');  // all variables
+  Rewrite(fo1);
+  Assign(fo2,'vsxb.dat');  // bright variables
+  Rewrite(fo2);
+
+  repeat
+    readln(fi,row);
+
+    varflag:=copy(row,40,1);
+    if varflag<>'0' then continue;  // not a variable
+
+    writeln(fo1,row); // all variable
+
+    buf:=trim(copy(row,105,8))+' ';
+    band1:=buf[1];
+    buf:=trim(copy(row,128,8))+' ';
+    band2:=buf[1];
+    if band1 in ['J','H','K','L','M'] then continue;  // IR var
+    if band2 in ['J','H','K','L','M'] then continue;
+
+    m1:=StrToFloatDef(trim(copy(row,95,7)),-9999);
+    if m1>=13.0 then continue;  // too faint
+    m2:=StrToFloatDef(trim(copy(row,118,7)),-9999);
+    if (m1<-100)or(m2<-100) then continue; // missing magnitude
+
+    mflag:=copy(row,114,1);
+    if mflag='Y' then  // amplitude
+      m2:=m1+m2;
+
+    if m2-m1 < 0.5 then continue;  // small amplitude
+
+    writeln(fo2,row); // bright visual variable
+
+  until eof(fi);
+
+  Close(fi);
+  Close(fo1);
+  Close(fo2);
+
+end.
+
